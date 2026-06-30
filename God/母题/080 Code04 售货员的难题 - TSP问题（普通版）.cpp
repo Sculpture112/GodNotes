bd496faa755c23e56e89dd3664db00da6a5e0f09
xxx <1748701882@qq.com>
2026-06-30 11:41:38 +0800

    vault backup: 2026-06-30 11:41:38

--- God/母题/080 Code04 售货员的难题 - TSP问题（普通版）.cpp
+++ God/母题/080 Code04 售货员的难题 - TSP问题（普通版）.cpp
@@ -6,7 +6,13 @@ const int INF = 0x3f3f3f3f;
 int n;
 int graph[MAXN][MAXN];
 int dp[1 << MAXN][MAXN];
-
+void build(){
+    for (int i = 0; i < n;i++){
+        for (int j = 0; j < n;j++){
+            graph[i][j] = -1;
+        }
+    }
+}
 int f(int s, int i)
 {
     if (s == (1 << n) - 1)
@@ -48,6 +54,7 @@ int main()
             cin >> graph[i][j];
         }
     }
+
 
     cout<<solve();
 
