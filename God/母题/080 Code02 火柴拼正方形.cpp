7c1171fa0deb025f55afd2fed4b973b37d473730
xxx <1748701882@qq.com>
2026-06-30 10:18:36 +0800

    vault backup: 2026-06-30 10:18:36

--- God/母题/080 Code02 火柴拼正方形.cpp
+++ God/母题/080 Code02 火柴拼正方形.cpp
@@ -4,7 +4,44 @@ using namespace std;
 class Solution
 {
 public:
-    bool makesquare(vector<int> &matchsticks)
+    bool makesquare(vector<int> &nums)
     {
+        int sum = 0;
+        for (int num : nums)
+        {
+            sum += num;
+        }
+        if (sum % 4 != 0)
+        {
+            return false;
+        }
+
+        int n = nums.size();
+        vector<int> dp(1 << n, 0);
+        return f(nums, sum / 4, (1 << n) - 1, 0, 4, dp);
+    }
+    bool f(vector<int> &nums, int limit, int status, int cur, int rest, vector<int> dp){
+        if(rest == 0){
+            return true;
+        }
+
+        bool ans = false;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(((status &(1<<i))!=0) && cur + nums[i] <=limit){
+                if(cur+nums[i] == limit){
+                    ans = f(nums, limit, (status ^ (1 << i)), 0, rest - 1, dp);
+                }
+                else{
+                    ans = f(nums, limit, (status ^ (1 << i)), cur + nums[i], rest, dp);
+                }
+
+                if(ans){
+                    break;
+                }
+            }
+        }
+        dp[status] = ans ? 1 : -1;
+        return ans;
     }
 };
