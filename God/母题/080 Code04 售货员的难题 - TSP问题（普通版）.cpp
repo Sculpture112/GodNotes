eca6b098d7a211a17d5a3d72855a0cbbbc9176e6
xxx <1748701882@qq.com>
2026-06-30 11:31:08 +0800

    vault backup: 2026-06-30 11:31:08

--- God/母题/080 Code04 售货员的难题 - TSP问题（普通版）.cpp
+++ God/母题/080 Code04 售货员的难题 - TSP问题（普通版）.cpp
@@ -6,14 +6,29 @@ const int INF = 0x3f3f3f3f;
 int n;
 int graph[MAXN][MAXN];
 int dp[1 << MAXN][MAXN];
-void solve()
+
+int f(int s,int i){
+    if(s == (1<<n)-1)
+}
+
+int solve()
 {
+    return f(1,1);
 }
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> graph[i][j];
+        }
+    }
 
     solve();
 
