a72880066774d38e3c3f0d71538bbcbd1ead8548
xxx <1748701882@qq.com>
2026-06-30 11:25:17 +0800

    vault backup: 2026-06-30 11:25:17

--- God/母题/080 Code04 售货员的难题 - TSP问题（普通版）.cpp
+++ God/母题/080 Code04 售货员的难题 - TSP问题（普通版）.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MAXN = 20;
+const int INF = 0x3f3f3f3f;
 
-void solve() {
-    
+int n;
+int graph[MAXN][MAXN];
+int dp[1 << MAXN][MAXN];
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
