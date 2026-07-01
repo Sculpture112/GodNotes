969b0b3d5aae51559756843abbadb1b1185c921d
xxx <1748701882@qq.com>
2026-07-01 10:11:58 +0800

    vault backup: 2026-07-01 10:11:58

--- God/母题/081 Code01 每个人戴不同帽子的方案数.cpp
+++ God/母题/081 Code01 每个人戴不同帽子的方案数.cpp
@@ -16,6 +16,40 @@ public:
         int n = arr.size();
 
         vector<int> hats(m + 1, 0);
-        
+        for (int pi = 0; pi < n;pi++){
+            for(int hat : arr[pi]){
+                hats[hat] |= (1 << pi);
+            }
+        }
+
+        vector<vector<int>> dp(m + 1, vector<int>((1 << n), -1));
+        return f(hats, m, n, 1, 0, dp);
+    }
+
+    int f(vector<int>& hats,int m,int n,int i,int s,vector<vector<int>>& dp){
+        if(s == (1<<n)-1){
+            return 1;
+        }
+
+        if(i == m+1){
+            return 0;
+        }
+
+        if(dp[i][s] != -1){
+            return dp[i][s];
+        }
+
+        int ans = f(hats, m, n, i + 1, s, dp);
+        int cur = hats[i];
+
+        while(cur!=0){
+            int rightone = cur & -cur;
+            if((s&rightone) == 0){
+                ans += f(hats, m, n, i + 1, s ^ rightone,dp);
+            }
+            cur ^= rightone;
+        }
+        dp[i][s] = ans;
+        return ans;
     }
 };
