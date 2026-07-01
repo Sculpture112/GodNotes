c7ed44ce76569cea1823b68af9fffd9594680a50
xxx <1748701882@qq.com>
2026-07-01 09:59:59 +0800

    vault backup: 2026-07-01 09:59:59

--- God/母题/081 Code01 每个人戴不同帽子的方案数.cpp
+++ God/母题/081 Code01 每个人戴不同帽子的方案数.cpp
@@ -4,7 +4,18 @@ using namespace std;
 class Solution
 {
 public:
-    int numberWays(vector<vector<int>> &hats)
+    int numberWays(vector<vector<int>> &arr)
     {
+        int m = 0;
+        for(auto& person : arr){
+            for(int hat : person){
+                m = max(m, hat);
+            }
+        }
+
+        int n = arr.size();
+
+        vector<int> hats(m + 1, 0);
+        
     }
 };
