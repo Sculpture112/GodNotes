c165c4e04a5c511a57a505ea854e6a8e53775d2d
xxx <1748701882@qq.com>
2026-07-01 15:03:06 +0800

    vault backup: 2026-07-01 15:03:06

--- God/母题/081 Code02 最优账单平衡.cpp
+++ God/母题/081 Code02 最优账单平衡.cpp
@@ -8,7 +8,8 @@ public:
     {
         vector<int> debt = debts(transactions);
         int n = debt.size();
-        
+        vector<int> dp((1 << n), -1);
+        return n - f(debt, n, 0, (1 << n) - 1, dp);
     }
     vector<int> debts(vector<vector<int>> &transaction)
     {
@@ -20,11 +21,42 @@ public:
         }
 
         vector<int> debt;
-        for(int num : help){
-            if(num!=0){
+        for (int num : help)
+        {
+            if (num != 0)
+            {
                 debt.push_back(num);
             }
         }
         return debt;
+    }
+    int f(vector<int> &debt, int n, int sum, int s, vector<int> &dp)
+    {
+        if (dp[s] != -1)
+        {
+            return dp[s];
+        }
+
+        int ans = 0;
+        if ((s & (s - 1)) != 0)
+        {
+            if(sum ==0){
+                for (int i = 0; i < n;i++){
+                    if((s & (1<<i)) != 0){
+                        ans = f(debt, n, sum - debt[i], s ^ (1 << i), dp) + 1;
+                        break;
+                    }
+                }
+            }
+            else{
+                for (int i = 0; i < n;i++){
+                    if((s & (1<<i)) != 0){
+                        ans = max(ans,f(debt, n, sum - debt[i], s ^ (1 << i), dp));
+                    }
+                }
+            }
+        }
+        dp[s] = ans;
+        return ans;
     }
 };
