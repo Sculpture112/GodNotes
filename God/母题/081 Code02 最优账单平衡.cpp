cf91c1f2e13a3c700b5e8f3f0bfd9657d7bac35a
xxx <1748701882@qq.com>
2026-07-01 14:49:20 +0800

    vault backup: 2026-07-01 14:49:20

--- God/母题/081 Code02 最优账单平衡.cpp
+++ God/母题/081 Code02 最优账单平衡.cpp
@@ -6,9 +6,17 @@ class Solution
 public:
     int minTransfers(vector<vector<int>> &transactions)
     {
-
     }
-    vector<int> dept(vector<vector<int>>& transaction){
-        for (int i = 0;i<n)
+    vector<int> dept(vector<vector<int>> &transaction)
+    {
+        vector<int> help(13, 0);
+        for (auto &tran : transaction)
+        {
+            help[tran[0]] -= tran[2];
+            help[tran[1]] += tran[2];
+        }
+
+        vector<int> help;
+        for()
     }
 };
