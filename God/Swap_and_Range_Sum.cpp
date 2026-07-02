6c3c522a6c28f7d733378258701f99e1d5d414c4
xxx <1748701882@qq.com>
2026-07-02 20:23:16 +0800

    vault backup: 2026-07-02 20:23:16

---
+++ God/Swap_and_Range_Sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+const int MAXN = 200001;
+int tree[MAXN];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    
+
+    return 0;
+}
