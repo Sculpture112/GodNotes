17336afd889bda1791d4612433552eab9b2f4a48
xxx <1748701882@qq.com>
2026-07-02 20:24:38 +0800

    vault backup: 2026-07-02 20:24:38

--- God/Swap_and_Range_Sum.cpp
+++ God/Swap_and_Range_Sum.cpp
@@ -4,12 +4,19 @@ int n, m;
 const int MAXN = 200001;
 int tree[MAXN];
 
+void add (int i,int v){
+    
+}
+
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
-    
+    for (int i = 1; i <= n; i++)
+    {
+    }
 
     return 0;
 }
