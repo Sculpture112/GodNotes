cfc4cbed05ced71481b7c649fb451cc14f02a927
xxx <1748701882@qq.com>
2026-07-02 15:13:37 +0800

    vault backup: 2026-07-02 15:13:37

--- God/母题/080状压526. 优美的排列.cpp
+++ God/母题/080状压526. 优美的排列.cpp
@@ -6,7 +6,7 @@ class Solution
 public:
     int countArrangement(int n)
     {
-        vector<int> dp(1 << n, 0);
+        vector<int> dp(1 << n, -1);
         return f((1 << n) - 1, 1, dp, n);
     }
     int f(int status, int index, vector<int> &dp, int n)
@@ -16,17 +16,21 @@ public:
             return 1;
         }
 
-        if (index == n)
+        if (index == n+1)
         {
             return 0;
+        }
+
+        if(dp[status] != -1){
+            return dp[status];
         }
 
         int ans = 0;
         for (int i = 1; i <= n; i++)
         {
-            if ((status & (1 << i)) != 0 && ((i % index == 0) || (index % i == 0)))
+            if ((status & (1 << (i - 1))) != 0 && ((i % index == 0) || (index % i == 0)))
             {
-                ans += f(status ^ (1 << i), index + 1, dp, n);
+                ans += f(status ^ (1 << (i-1)), index + 1, dp, n);
             }
         }
         dp[status] = ans;
