4196d7e92a74f6ec89a6a0b1df1e4ae8ff25372d
xxx <1748701882@qq.com>
2026-07-02 20:21:28 +0800

    vault backup: 2026-07-02 20:21:28

--- God/P_3374_模板_树状数组_1.cpp
+++ God/P_3374_模板_树状数组_1.cpp
@@ -1,15 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MAXN = 500001;
+int n, m;
 
-void solve() {
-    
+int tree[MAXN];
+int lowbit(int i)
+{
+    return i & -i;
+}
+void add(int i, int v)
+{
+    while (i <= n)
+    {
+        tree[i] += v;
+        i += lowbit(i);
+    }
 }
 
-int main() {
+int sum(int i){
+    int ans = 0;
+    while(i>0){
+        ans += tree[i];
+        i -= lowbit(i);
+    }
+    return ans;
+}
+
+int edgesum(int i,int j ){
+    return sum(j) - sum(i-1);
+}
+
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cin >> n >> m;
+    for (int i = 1, v; i <= n; i++)
+    {
+        cin >> v;
+        add(i, v);
+    }
 
-    solve();
+    for (int i = 1,a,b,c; i <= m;i++){
+        cin >> a >> b >> c;
+        if(a == 1){
+            add(b, c);
+        }
+        else{
+            cout << edgesum(b, c) << "\n";
+
+        }
+    }
+
 
     return 0;
 }
--- God/vjudge.net/contest/826612#problem/A/a.cpp
+++ God/vjudge.net/contest/826612#problem/A/a.cpp
@@ -1,56 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[200005];
 
-int main()
-{
+void solve() {
+    
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, q;
-    cin >> n >> q;
 
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> arr[i];
-    }
-    int query[q + 1][3];
-    for (int i = 1; i <= q; i++)
-    {
-        int temp;
-        cin >> temp;
-        query[i][0] = temp;
-        if (temp == 1)
-        {
-            cin >> query[i][1];
-        }
-        else
-        {
-            cin >> query[i][1] >> query[i][2];
-        }
-    }
-
-    for (int i = 1; i <= q; i++)
-    {
-        if (query[i][0] == 1)
-        {
-            int t = query[i][1];
-            // cout << arr[t] << " " << arr[t + 1] << "\n";
-            int temp = arr[t];
-            arr[t] = arr[t + 1];
-            arr[t + 1] = temp;
-            // cout << arr[t] << " " << arr[t + 1] <<"\n";
-        }
-        else
-        {
-            int l = query[i][1], r = query[i][2];
-            // cout << l << " " << r << "\n";
-            int sum = 0;
-            for (int i = l; i <= r;i++){
-                sum += arr[i];
-            }
-            cout << sum << "\n";
-        }
-    }
+    solve();
 
     return 0;
 }
