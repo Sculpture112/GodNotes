2af5d975903e0f19fd85960f8fb98486efec7200
xxx <1748701882@qq.com>
2026-07-02 16:55:40 +0800

    vault backup: 2026-07-02 16:55:40

--- God/母题/080状压1986. 完成任务的最少工作时间段.cpp
+++ God/母题/080状压1986. 完成任务的最少工作时间段.cpp
@@ -6,5 +6,35 @@ class Solution
 public:
     int minSessions(vector<int> &tasks, int sessionTime)
     {
+        int n = tasks.size();
+        int size = 1 << n;
+
+        vector<int> sum(size, 0);
+
+        for (int status = 1; status < size; status++)
+        {
+            int rightone = status & -status;
+            int i = __builtin_ctz(rightone);
+            sum[status] = sum[status ^ rightone] + tasks[i];
+        }
+
+        const int INF = 0x3f3f3f3f;
+        vector<int> dp(size, INF);
+
+        dp[0] = 0;
+
+        for (int statu = 1; statu < size; statu++)
+        {
+            for (int sub = statu; sub > 0; sub = (sub - 1) & statu)
+            {
+                if (sum[sub] <= sessionTime)
+                {
+                    dp[statu] = min(dp[statu], dp[statu ^ sub] + 1);
+                }
+            }
+        }
+        
+        
+        return dp[size-1];
     }
 };
