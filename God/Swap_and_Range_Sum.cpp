71553004d0083bdfae9eb29e8bf1ce20ed1b40fc
xxx <1748701882@qq.com>
2026-07-02 20:27:57 +0800

    vault backup: 2026-07-02 20:27:57

--- God/Swap_and_Range_Sum.cpp
+++ God/Swap_and_Range_Sum.cpp
@@ -4,28 +4,50 @@ int n, m;
 const int MAXN = 200001;
 int tree[MAXN];
 
-
-int lowbit(int i ){
+int lowbit(int i)
+{
     return i & -i;
 }
 
-void add (int i,int v){
-    while(i<=n){
+void add(int i, int v)
+{
+    while (i <= n)
+    {
         tree[i] += v;
         i += lowbit(i);
     }
 }
 
-void range(int)
+int sum(int i)
+{
+    int ans = 0;
+    while (i > 0)
+    {
+        ans += tree[i];
+        i -= lowbit(i);
+    }
+    return ans;
+}
 
+int range(int l, int r)
+{
+    return sum(r) - sum(l - 1);
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1, v; i <= n; i++)
     {
+        cin >> v;
+        add(i, v);
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        
     }
 
     return 0;
