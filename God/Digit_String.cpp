c13bb70fc7918e2c6107b97d4d9f013e3a3ef770
xxx <1748701882@qq.com>
2026-07-03 09:28:56 +0800

    vault backup: 2026-07-03 09:28:56

--- God/Digit_String.cpp
+++ God/Digit_String.cpp
@@ -14,15 +14,27 @@ int main() {
     while(t--){
         string s;
         int n = s.size();
+        int sum13 = 0;
+        for(char c:s){
+            if(c == '1' || c== '3'){
+                sum13++;
+            }
+        }
+        int sum2 = 0;
+        int ans = 0;
         for(char c:s){
             if(c == 2){
-
+                sum2++;
             }
-
+            else{
+                sum13--;
+            }
+            ans = max(ans, sum2 + sum13);
         }
+        cout << ans << "\n";
     }
 
-    solve();
+
 
     return 0;
 }
