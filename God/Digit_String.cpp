053957330eb64852d398d1af3754194d6b75cf90
xxx <1748701882@qq.com>
2026-07-03 09:27:12 +0800

    vault backup: 2026-07-03 09:27:11

--- God/Digit_String.cpp
+++ God/Digit_String.cpp
@@ -8,6 +8,19 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int t;
+    cin >> t;
+
+    while(t--){
+        string s;
+        int n = s.size();
+        for(char c:s){
+            if(c == 2){
+
+            }
+
+        }
+    }
 
     solve();
 
