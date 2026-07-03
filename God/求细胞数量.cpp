f450a4fc73672558c0126764565985348c406289
xxx <1748701882@qq.com>
2026-07-03 16:16:38 +0800

    vault backup: 2026-07-03 16:16:38

--- God/求细胞数量.cpp
+++ God/求细胞数量.cpp
@@ -5,13 +5,16 @@ int cell = 0;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0, -1, 0, 1};
 
-void dfs(int x,int y)
+void dfs(int x,int y,vector<vector<int>>& grid)
 {
     for (int i = 0; i < 4;i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
         if(nx<0 || nx>=n || ny<0||ny>=m) continue;
-        
+        if(grid[nx][ny] ==0) continue;
+
+        grid[nx][ny] = 0;
+        dfs()
     }
 }
 
