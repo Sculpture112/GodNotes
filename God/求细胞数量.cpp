360c4c823bfaf223b7c4ce7e4a1c3cb13d1855f1
xxx <1748701882@qq.com>
2026-07-03 16:17:46 +0800

    vault backup: 2026-07-03 16:17:46

--- God/求细胞数量.cpp
+++ God/求细胞数量.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 int n, m;
 int cell = 0;
-int dx[4] = {1,0,-1,0};
+int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-void dfs(int x,int y,vector<vector<int>>& grid)
+void dfs(int x, int y, vector<vector<int>> &grid)
 {
-    for (int i = 0; i < 4;i++){
+    for (int i = 0; i < 4; i++)
+    {
         int nx = x + dx[i];
         int ny = y + dy[i];
-        if(nx<0 || nx>=n || ny<0||ny>=m) continue;
-        if(grid[nx][ny] ==0) continue;
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+            continue;
+        if (grid[nx][ny] == 0)
+            continue;
 
         grid[nx][ny] = 0;
-        dfs()
+        dfs(nx, ny, grid);
     }
 }
 
@@ -24,21 +27,27 @@ int main()
     cin.tie(nullptr);
     cin >> n >> m;
     vector<vector<int>> grid(n, vector<int>(m));
-    for (int i = 0; i < n;i++){
-        for (int j = 0; j < m;j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             cin >> grid[i][j];
         }
     }
 
-    for (int i = 0; i < n;i++){
-        for (int j = 0; j < m;j++){
-            if(grid[i][j] != 0){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] != 0)
+            {
                 cell++;
                 grid[i][j] = 0;
-                dfs(i, j);
+                dfs(i, j, grid);
             }
         }
     }
+    cout << cell;
 
-        return 0;
+    return 0;
 }
