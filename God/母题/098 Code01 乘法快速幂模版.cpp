6c1d327e1b2f541908e0fb4eeada5ad8c0b519f8
xxx <1748701882@qq.com>
2026-07-03 08:57:35 +0800

    vault backup: 2026-07-03 08:57:34

--- God/母题/098 Code01 乘法快速幂模版.cpp
+++ God/母题/098 Code01 乘法快速幂模版.cpp
@@ -1,15 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    
+long long a, b, p;
+
+long long power(){
+    long long ans = 1;
+    while(b>0){
+        if((b&1) == 1){
+            ans = (ans * a)%p;
+        }
+        a = (a * a) % p;
+        b >>= 1;
+    }
+    return ans;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    solve();
+    cin >> a >> b >> p;
+    cout << a << "^" << b << " mod " << p << "=" << power();
 
     return 0;
 }
