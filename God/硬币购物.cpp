f48bcbd88e6f8e7fee8cec097c8f80bb3c6cbe4a
xxx <1748701882@qq.com>
2026-07-03 14:28:53 +0800

    vault backup: 2026-07-03 14:28:53

--- God/硬币购物.cpp
+++ God/硬币购物.cpp
@@ -1,45 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int v[5], c[5];
-int n, s;
 
-int main()
-{
+void solve() {
+    
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    for (int i = 1; i <= 4; i++)
-    {
-        cin >> v[i];
-    }
 
-    cin >> n;
-
-    while (n--)
-    {
-        for (int i = 1; i <= 4; i++)
-        {
-            cin >> c[i];
-        }
-        cin >> s;
-        // 前i个硬币，凑出块钱j的方法数
-        vector<vector<long long>> dp(5, vector<long long>(s + 1, 0));
-        dp[0][0] = 1;
-        for (int i = 1; i <= 4; i++)
-        {
-            for (int j = 0; j <= s; j++)
-            {
-                dp[i][j] = dp[i - 1][j];
-                for (int k = 1; k <= c[i]; k++)
-                {
-                    if (k * v[i] <= j)
-                    {
-                        dp[i][j] += dp[i - 1][j - k * v[i]];
-                    }
-                }
-            }
-        }
-        cout << dp[4][s] << "\n";
-    }
+    solve();
 
     return 0;
 }
