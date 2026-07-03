75b2ab73d611e437345ec065a29898acf09ee89b
xxx <1748701882@qq.com>
2026-07-03 15:19:56 +0800

    vault backup: 2026-07-03 15:19:56

--- God/P_1443_马的遍历.cpp
+++ God/P_1443_马的遍历.cpp
@@ -1,15 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    
-}
-
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+    vector<vector<int>> dist(n + 1, vector<int>(m + 1, -1));
 
-    solve();
+    queue<pair<int, int>> q;
+    dist[x][y] = 0;
+    q.push({x, y});
+    while (!q.empty())
+    {
+        auto [cx, cy] = q.front();
+        q.pop();
 
-    return 0;
+        int dx[8] = {2, 2, 1, -1, -2, -2, -1, 1};
+        int dy[8] = {1, -1, -2, -2, -1, 1, 2, 2};
+        for (int i = 0; i < 8; i++)
+        {
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+
+            if (nx < 1 || nx > n || ny < 1 || ny > m)
+            {
+                continue;
+            }
+            if (dist[nx][ny] != -1)
+            {
+                continue;
+            }
+            dist[nx][ny] = dist[cx][cy] + 1;
+            q.push({nx, ny});
+        }
+    }
+
+    for (int i = 1; i <= n;i++){
+        for (int j = 1; j <= m;j++){
+            cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+        return 0;
 }
