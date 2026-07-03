56d3a4eab8b666fa4c9502d353f9f3490a9dfe6c
xxx <1748701882@qq.com>
2026-07-03 22:20:50 +0800

    vault backup: 2026-07-03 22:20:50

--- God/勤.cpp
+++ God/勤.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int n, a, b;
 void solve() {
     
 }
@@ -8,6 +8,12 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cin >> n >> a >> b;
+    if(a+b<n){
+        cout << 0;
+        return 0;
+    }
+    cout << abs(b - (n - a));
 
     solve();
 
