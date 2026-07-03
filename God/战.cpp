a58dbd5922de597da4d7bae3e823307a7c229803
xxx <1748701882@qq.com>
2026-07-03 23:03:27 +0800

    vault backup: 2026-07-03 23:03:27

--- God/战.cpp
+++ God/战.cpp
@@ -3,20 +3,29 @@ using namespace std;
 int arr[20005];
 
 int n;
-bool find9(){
+bool find9()
+{
 
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] != 0)
+        if (arr[i] != -1)
         {
-            for (int j = i + 1; j < n;j++){
-                if(arr[i][j])
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] + arr[j] ==9)
+                {
+                    arr[i] = -1;
+                    arr[j] = -1;
+                    return true;
+                }
             }
         }
     }
+    return false;
 }
 bool f()
 {
+    
 }
 
 int main()
