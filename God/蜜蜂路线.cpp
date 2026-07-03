50b38d68af745ec685fff192bf02a90fd1d41163
xxx <1748701882@qq.com>
2026-07-03 16:43:10 +0800

    vault backup: 2026-07-03 16:43:10

--- God/蜜蜂路线.cpp
+++ God/蜜蜂路线.cpp
@@ -10,7 +10,7 @@ int main()
     cin >> m >> n;
     dp[m] = 1;
     dp[m + 1] = 1;
-    for (int i = m + 1; i<=n; i++)
+    for (int i = m + 2; i<=n; i++)
     {
         dp[i] = dp[i - 1] + dp[i - 2];
     }
