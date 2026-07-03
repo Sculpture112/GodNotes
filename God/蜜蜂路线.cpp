74106855a8a5832ab980927fe85d740c4cb86838
xxx <1748701882@qq.com>
2026-07-03 17:04:22 +0800

    vault backup: 2026-07-03 17:04:22

--- God/蜜蜂路线.cpp
+++ God/蜜蜂路线.cpp
@@ -1,21 +1,41 @@
 #include <bits/stdc++.h>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace std;
-using boost::multiprecision::cpp_int;
-
 int m, n;
-cpp_int dp[1001];
+string dp[1005];
+string add(string a, string b)
+{
+    string ans;
+    int carry = 0;
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+
+    while (i >= 0 || j >= 0 || carry)
+    {
+        int sum = carry;
+        if (i >= 0)
+        {
+            sum += a[i--] - '0';
+        }
+        if (j >= 0)
+            sum += b[j--] - '0';
+        ans.push_back(sum % 10 + '0');
+        carry =sum / 10;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> m >> n;
-    dp[m] = 1;
-    dp[m + 1] = 1;
-    for (int i = m + 2; i<=n; i++)
+    dp[m] = '1';
+    dp[m + 1] = '1';
+
+    for (int i = m + 2; i <= n; i++)
     {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] = add(dp[i - 1], dp[i - 2]);
     }
     cout << dp[n];
 
