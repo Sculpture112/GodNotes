b15e56609a746ff09a44565a91e2a1ece7a39e75
xxx <1748701882@qq.com>
2026-07-03 14:44:05 +0800

    vault backup: 2026-07-03 14:44:05

--- God/硬币购物.cpp
+++ God/硬币购物.cpp
@@ -1,15 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void solve() {
-    
-}
-
-int main() {
+int v[4];
+int n;
+using ll = long long;
+const int MAXN = 100005;
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> v[i];
+    }
 
-    solve();
+    vector<ll> f(MAXN, 0); // 每个硬币无限次凑出j的方案数
+    f[0] = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = v[i]; j < MAXN; j++)
+        {
+            f[j] += f[j - v[i]];
+        }
+    }
+
+    cin >> n;
+    while (n--)
+    {
+        int c[4];
+        for (int i = 0; i < 4; i++)
+        {
+            cin >> c[i];
+        }
+        int s;
+        cin >> s;
+
+        int rest = s;
+
+        ll ans = 0;
+        for (int mask = 0; mask < 16; mask++)
+        {
+
+            int cnt = 0;
+            for (int i = 0; i < 4; i++)
+            {
+
+                if (mask & (1 << i) != 0)
+                {
+                    rest -= (c[i] + 1) * v[i];
+                    cnt++;
+                }
+            }
+
+            if (rest < 0)
+            {
+                continue;
+            }
+
+            if(cnt %2 ==0){
+                ans += f[rest];
+            }
+            else{
+                ans -= f[rest];
+            }
+        }
+    }
 
     return 0;
 }
