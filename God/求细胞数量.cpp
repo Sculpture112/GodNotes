11044a8214389ff391845e067672faea78525260
xxx <1748701882@qq.com>
2026-07-03 16:12:04 +0800

    vault backup: 2026-07-03 16:12:03

---
+++ God/求细胞数量.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    vector<vector<int>> grid(n, vector<int>(m));
+    for (int i =)
+
+        solve();
+
+    return 0;
+}
