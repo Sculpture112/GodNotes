d8aa7494266dd32f2bcb4baacc3ca4b301f01ba7
xxx <1748701882@qq.com>
2026-07-03 16:22:20 +0800

    vault backup: 2026-07-03 16:22:20

--- God/求细胞数量.cpp
+++ God/求细胞数量.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<string> grid;
 int n, m;
 int cell = 0;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-void dfs(int x, int y, vector<vector<int>> &grid)
+void dfs(int x, int y)
 {
-    grid[x][y] = 0;
+    grid[x][y] = '0';
     for (int i = 0; i < 4; i++)
     {
         int nx = x + dx[i];
         int ny = y + dy[i];
         if (nx < 0 || nx >= n || ny < 0 || ny >= m)
             continue;
-        if (grid[nx][ny] == 0)
+        if (grid[nx][ny] == '0')
             continue;
 
-        grid[nx][ny] = 0;
-        dfs(nx, ny, grid);
+        grid[nx][ny] = '0';
+        dfs(nx, ny);
     }
 }
 
@@ -27,24 +28,21 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
-    vector<vector<int>> grid(n, vector<int>(m));
+    grid.resize(n);
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> grid[i][j];
-        }
+        cin >> grid[i];
     }
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            if (grid[i][j] != 0)
+            if (grid[i][j] != '0')
             {
                 cell++;
                 
-                dfs(i, j, grid);
+                dfs(i, j);
             }
         }
     }
