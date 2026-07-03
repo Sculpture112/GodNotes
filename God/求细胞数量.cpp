1771f828035efddd85749755633c14daff7645ad
xxx <1748701882@qq.com>
2026-07-03 16:13:37 +0800

    vault backup: 2026-07-03 16:13:37

--- God/求细胞数量.cpp
+++ God/求细胞数量.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
+int cell = 0;
 
-void solve()
+void dfs(int x,int y)
 {
+
 }
 
 int main()
@@ -12,9 +14,21 @@ int main()
     cin.tie(nullptr);
     cin >> n >> m;
     vector<vector<int>> grid(n, vector<int>(m));
-    for (int i =)
+    for (int i = 0; i < n;i++){
+        for (int j = 0; j < m;j++){
+            cin >> grid[i][j];
+        }
+    }
 
-        solve();
+    for (int i = 0; i < n;i++){
+        for (int j = 0; j < m;j++){
+            if(grid[i][j] != 0){
+                cell++;
+                grid[i][j] = 0;
+                dfs(i, j);
+            }
+        }
+    }
 
-    return 0;
+        return 0;
 }
