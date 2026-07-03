e9e0b7178d208eccaaec4b054f5a3c7d1788a28b
xxx <1748701882@qq.com>
2026-07-03 16:28:41 +0800

    vault backup: 2026-07-03 16:28:40

---
+++ God/蜜蜂路线.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int m, n;
+int dfs(int x)
+{
+    if (x == n)
+    {
+        return 1;
+    }
+
+    if()
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> m >> n;
+
+    return 0;
+}
