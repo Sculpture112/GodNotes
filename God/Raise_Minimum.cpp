95a3b8af828a9cf7606d9af0a99bba853e6973d7
xxx <1748701882@qq.com>
2026-07-03 10:14:25 +0800

    vault backup: 2026-07-03 10:14:25

--- God/Raise_Minimum.cpp
+++ God/Raise_Minimum.cpp
@@ -1,18 +1,56 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void solve() {
-    
+using ll = long long;
+
+bool ok(vector<ll>& A,ll k,ll x){
+    ll need = 0;
+    int n = A.size();
+
+    for (int idx = 0; idx < n;idx++){
+        int i = idx + 1;
+
+        if(A[idx] < x){
+            ll diff = x - A[idx];
+            need += (diff + i - 1) / i;
+
+            if(need > k){
+                return false;
+            }
+        }
+    }
+    return need <= k;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, k;
+    int n;
+    ll k;
     cin >> n >> k;
-    
+    vector<ll> A(n);
+    ll mn = LONG_LONG_MAX;
 
-    solve();
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+        mn = min(mn, A[i]);
+    }
+
+    ll left = mn, right = mn + n * k + 1;
+
+    while(right - left >1){
+        ll mid = (left - right) / 2 + right;
+        if(ok(A,k,mid)){
+            left = mid;
+        }
+        else{
+            right = mid;
+        }
+    }
+    cout << left;
 
     return 0;
 }
