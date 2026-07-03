a78a70ff7479315fc9fe658ec2476aeb294c498a
xxx <1748701882@qq.com>
2026-07-03 22:58:22 +0800

    vault backup: 2026-07-03 22:58:22

---
+++ God/战.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[20005];
+
+int n;
+bool f()
+{
+    if()
+    
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+    }
+
+    return 0;
+}
