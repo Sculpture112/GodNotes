5f9eaab96c0ac5253bf78cc0adc9cf455f5f036b
xxx <1748701882@qq.com>
2026-07-04 14:28:26 +0800

    vault backup: 2026-07-04 14:28:26

---
+++ God/和为k.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int h, w, k;
+vector<string> grid;
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> h >> w >> k;
+    grid.resize(h);
+    for (int i = 0; i < h;i++){
+        cin >> grid[i];
+    }
+
+    vector<vector<int>> arr(h + 1, vector<int>(w + 1, 0));
+    for (int i = 1; i <= h;i++){
+        for (int j = 1; j <= w;j++){
+            arr[i][j] = grid[i - 1][j - 1]-'0';
+        }
+    }
+
+    for (int i = 1; i <= h;i++){
+        for (int j = 1; i <= w;j++){
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+        }
+    }
+    
+
+        return 0;
+}
