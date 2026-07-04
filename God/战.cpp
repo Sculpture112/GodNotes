721e70dc539bc89807babad216fb3f3c76b0b2ef
xxx <1748701882@qq.com>
2026-07-04 16:00:42 +0800

    vault backup: 2026-07-04 16:00:42

--- God/战.cpp
+++ God/战.cpp
@@ -1,52 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[20005];
 
-int n;
-bool find9()
-{
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] != -1)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (arr[i] + arr[j] ==9)
-                {
-                    arr[i] = -1;
-                    arr[j] = -1;
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
-bool f()
-{
-
-    if(find9() && !f()){
-        return true;
-    }
-    return false;
+void solve() {
+    
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;
     cin >> t;
-
-    while (t--)
-    {
+    while(t--){
+        int n;
         cin >> n;
-
-        for (int i = 0; i < n; i++)
+        vector<int> arr(n + 1);
+        for (int i = 0; i < n;i++){
             cin >> arr[i];
+        }
+
+        int cnt = 0;
+
+        vector<int> st;
+        for (int i = 0; i < n;i++){
+            int x = arr[i]%9;
+            
+            if(!st.empty() && (x + st.back())%9 == 0)
+            {
+                cnt++;
+                st.pop_back();
+            }
+            else{
+                st.push_back(x);
+            }
+        }
     }
-    cout<<f();
+    solve();
 
     return 0;
 }
