9b33e75e36ef5f60558e18ec5b7e4e827b6bde0c
xxx <1748701882@qq.com>
2026-07-04 15:32:08 +0800

    vault backup: 2026-07-04 15:32:07

--- God/积.cpp
+++ God/积.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int t;
+vector<int> arr;
 void solve() {
     
 }
@@ -8,8 +9,39 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        arr.resize(n);
+        bool haszero = false;
+        for (int i = 0; i < n;i++){
+            cin >> arr[i];
+            if(arr[i] == 0)
+                haszero = true;
+        }
 
-    solve();
+        if(haszero){
+            cout << 0 << "\n";
+            continue;
+        }
+
+        vector<int> cost;
+        int ans = LLONG_MAX;
+        for (int i = 0; i < n;i++){
+            int x = arr[i];
+
+            int c3 = (3 - (x % 3)) % 3;
+            int c9 = (9 - (x % 9)) % 9;
+
+            ans = min(ans, c9);
+            cost.push_back(c3);
+        }
+
+        sort(cost.begin(), cost.end());
+        ans = min(ans, cost[0] + cost[1]);
+        cout << ans << "\n";
+    }
 
     return 0;
 }
