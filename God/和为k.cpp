c59c30148ac45c83ade4456bae67f20e116b9be0
xxx <1748701882@qq.com>
2026-07-04 14:37:27 +0800

    vault backup: 2026-07-04 14:37:27

--- God/和为k.cpp
+++ God/和为k.cpp
@@ -2,47 +2,51 @@
 using namespace std;
 long long h, w, k;
 vector<string> grid;
-void solve() {
-    
-}
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> h >> w >> k;
     grid.resize(h);
-    for (int i = 0; i < h;i++){
+    for (int i = 0; i < h; i++)
+    {
         cin >> grid[i];
     }
 
     vector<vector<int>> arr(h + 1, vector<int>(w + 1, 0));
-    for (int i = 1; i <= h;i++){
-        for (int j = 1; j <= w;j++){
-            arr[i][j] = grid[i - 1][j - 1]-'0';
+    for (int i = 1; i <= h; i++)
+    {
+        for (int j = 1; j <= w; j++)
+        {
+            arr[i][j] = grid[i - 1][j - 1] - '0';
         }
     }
 
-    for (int i = 1; i <= h;i++){
-        for (int j = 1; i <= w;j++){
+    for (int i = 1; i <= h; i++)
+    {
+        for (int j = 1; i <= w; j++)
+        {
             arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+            cout << arr[i][j];
         }
     }
 
-    long long ans = 0;
-    for (int i = 1; i <= h;i++){
-        for (int j = 1; j <= w;j++){
+    // long long ans = 0;
+    // for (int i = 1; i <= h;i++){
+    //     for (int j = 1; j <= w;j++){
 
-            for (int x = i; x >= 1;x--){
-                for (int y = j; y >= 1;y--){
+    //         for (int x = i; x >= 1;x--){
+    //             for (int y = j; y >= 1;y--){
 
-                    if(arr[i][j] -arr[i][y-1] -arr[x-1][j] + arr[x-1][y-1] == k){
-                        ans++;
-                    }
-                }
-            }
-        }
-    }
-    cout << ans;
+    //                 if(arr[i][j] -arr[i][y-1] -arr[x-1][j] + arr[x-1][y-1] == k){
+    //                     ans++;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // cout << ans;
 
     return 0;
 }
