d1e8ec2aebcf93b88ac4403882023ae670989c0e
xxx <1748701882@qq.com>
2026-07-04 14:14:27 +0800

    vault backup: 2026-07-04 14:14:27

---
+++ God/寻找宝藏.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k, m;
+const int MAXN = 20010;
+int value[MAXN];
+int cnt[MAXN];
+void solve()
+{
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> k >> m;
+    for (int i = 1; i <= n;i++){
+        cin >> cnt[i] >> value[i];
+    }
+
+        solve();
+
+    return 0;
+}
