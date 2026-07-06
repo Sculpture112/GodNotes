df928ef91af10ed3c0775df4e5ab452c791570a2
xxx <1748701882@qq.com>
2026-07-06 15:08:51 +0800

    vault backup: 2026-07-06 15:08:51

--- God/面试题 17.24. 最大子矩阵.cpp
+++ God/面试题 17.24. 最大子矩阵.cpp
@@ -15,15 +15,32 @@ public:
         int a = 0, b = 0, c = 0, d = 0;
         int maxsum = INT_MIN;
 
-        for (int a = 0; a < n;a++){
-            for (int c = a; c < n;c++){
+        for (int up = 0; up < n;up++){
+            for (int down = up; down < n;down++){
                 vector<int> nums(m, 0);
                 int pre = 0;
-                for (int j = 0;j<m;j++){
-                    nums[j] += matrix[c][j];
+                for (int r = 0,l = 0,pre = INT_MIN;r<m;r++){
+                    nums[r] += matrix[down][r];
+
+                    if(pre>=0){
+                        pre += nums[r];
+                    }
+                    else{
+                        pre = nums[r];
+                        l = r;
+                    }
+
+                    if(pre > maxsum){
+                        maxsum = pre;
+                        a = up;
+                        b = l;
+                        c = down;
+                        d = r;
+                    }
 
                 }
             }
         }
+        return{a,b,c,d};
     }
 };
