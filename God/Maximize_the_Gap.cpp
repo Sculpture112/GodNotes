db1f39182049e2bdd0e9dcf16878f730185347f8
xxx <1748701882@qq.com>
2026-07-06 12:02:02 +0800

    vault backup: 2026-07-06 12:02:02

--- God/Maximize_the_Gap.cpp
+++ God/Maximize_the_Gap.cpp
@@ -8,7 +8,7 @@ int main()
     cin.tie(nullptr);
     int n, k;
     cin >> n >> k;
-    vector<pair<int, int>> seg;
+    vector<pair<int, int>> segs;
     for (int i = 0; i < n; i++)
     {
         int l, r;
@@ -16,15 +16,23 @@ int main()
         segs.push_back({l, r});
     }
 
-    auto check = [&](ll x){
+    auto check = [&](ll x)
+    {
         int lastr = -1;
         int cnt = 0;
-        for(auot seg : segs){
-            
-        })
-
-
-
+        for (auto [l, r] : segs)
+        {
+            if (cnt == 0 || l >= lastr + x)
+            {
+                cnt++;
+                lastr = r;
+                if (cnt >= k)
+                    return true;
+            }
+        }
+        return false;
     };
+
+    
     return 0;
 }
