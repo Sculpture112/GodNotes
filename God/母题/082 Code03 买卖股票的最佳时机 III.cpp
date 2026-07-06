2fbeb6556240ffd9a5fc4729784befd2f7f7de30
xxx <1748701882@qq.com>
2026-07-06 09:31:07 +0800

    vault backup: 2026-07-06 09:31:07

--- God/母题/082 Code03 买卖股票的最佳时机 III.cpp
+++ God/母题/082 Code03 买卖股票的最佳时机 III.cpp
@@ -1,15 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    
-}
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int dp1 = 0;
+        int best = -prices[0];
+        int ans = 0;
+        int mn = prices[0];
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}
+        for (int i = 0; i < prices.size(); i++)
+        {
+            mn = min(mn,prices[i]);
+            dp1 = max(dp1, prices[i] - mn);
+            best = max(best, dp1 - prices[i]);
+            ans = max(ans, best + prices[i]);
+        }
+        return ans;
+    }
+};
