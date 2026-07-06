13bac4d8aeabb31f9ac1e4942bfa8de86be3dd81
xxx <1748701882@qq.com>
2026-07-06 14:42:49 +0800

    vault backup: 2026-07-06 14:42:49

--- God/Count_Subgrid_Sum_K.cpp
+++ God/Count_Subgrid_Sum_K.cpp
@@ -2,15 +2,59 @@
 using namespace std;
 #define ll long long
 
-void solve() {
-    
-}
-
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int h, w, k;
+    cin >> h >> w >> k;
 
-    solve();
+    vector<string> s(h);
+    for (int i = 0; i < h; i++)
+    {
+        cin >> s[i];
+    }
+    ll ans = 0;
+    vector<int> freq(h * w + 1, 0);
+    for (int top = 0; top < h; top++)
+    {
+        vector<int> col(w, 0);
+        for (int bottow = top; bottow < h; bottow++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                col[j] += s[bottow][j] - '0';
+            }
+
+            vector<int> touched;
+            auto addfreq = [&](int x)
+            {
+                if (freq[x] == 0)
+                {
+                    touched.push_back(x);
+                }
+                freq[x]++;
+            };
+
+            addfreq(0);
+            int fresum = 0;
+            for (int j = 0; j < w; j++)
+            {
+                fresum += col[j];
+                if (fresum >= k)
+                {
+                    ans += freq[fresum - k];
+                }
+                addfreq(fresum);
+            }
+
+            for (int x : touched)
+            {
+                freq[x] = 0;
+            }
+        }
+    }
+    cout << ans;
 
     return 0;
 }
