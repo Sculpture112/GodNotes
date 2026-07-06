e2ff91c1f4b0b3fdb7b7637f0573a2572432e93b
xxx <1748701882@qq.com>
2026-07-06 11:11:59 +0800

    vault backup: 2026-07-06 11:11:59

--- God/Variety.cpp
+++ God/Variety.cpp
@@ -1,15 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    
-}
-
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    solve();
+    int n, k, m;
+    cin >> n >> k >> m;
+    vector<pair<int, int>> gem;
+
+    for (int i = 0; i < n; i++)
+    {
+        int c, v;
+        cin >> c >> v;
+        gem.push_back({v, c});
+    }
+
+    sort(gem.rbegin(), gem.rend());
+
+    vector<int> cnt(n + 1, 0);
+    vector<int> addable(n);
+    vector<int> removable(n);
+
+    int colors = 0;
+    int ans = 0;
+    for (int i = 0; i < k; i++)
+    {
+        auto [v, c] = gem[i];
+        ans += v;
+        cnt[c]++;
+        if (cnt[c] == 1)
+        {
+            colors++;
+
+        }
+        else
+        {
+            removable.push_back(v);
+        }
+    }
+
+    for (int i = k; i < n; i++)
+    {
+        auto [v, c] = gem[i];
+
+        if (cnt[i] == 0)
+        {
+            addable.push_back(v);
+            cnt[i] = -1;
+        }
+    }
+
+    sort(removable.begin(), removable.end());
+    sort(addable.rbegin(), addable.rend());
+
+    int i = 0, j = 0;
+    while(colors<m){
+        ans -= removable[i];
+        ans += addable[j];
+
+        i++, j++;
+        colors++;
+    }
+    cout << ans;
 
     return 0;
 }
