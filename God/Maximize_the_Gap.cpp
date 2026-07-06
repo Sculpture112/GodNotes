45ca0f5080d29132ab03938db9a3e5c9deadaade
xxx <1748701882@qq.com>
2026-07-06 11:57:12 +0800

    vault backup: 2026-07-06 11:57:12

--- God/Maximize_the_Gap.cpp
+++ God/Maximize_the_Gap.cpp
@@ -1,15 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long;
 
-void solve() {
-    
-}
-
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int, int>> seg;
+    for (int i = 0; i < n; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        seg.push_back({l, r});
+    }
 
-    solve();
+    auto check = [&](ll x){
+        int lastr = -1;
 
+    };
     return 0;
 }
