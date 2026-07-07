2b116794cc976e6de7d9f15094a356509e8f9881
xxx <1748701882@qq.com>
2026-07-07 16:31:15 +0800

    vault backup: 2026-07-07 16:31:15

--- God/计算系数.cpp
+++ God/计算系数.cpp
@@ -2,17 +2,49 @@
 using namespace std;
 #define ll long long
 ll a, b, k, n, m;
-int solve() {
-        
+const int MOD = 10007;
+ll ckn(ll k, ll n)
+{
+    ll ans = 1;
+    ll chu = 1;
+    for (int i = n; i >= 1; i--, k--)
+    {
+        ans *= k;
+        chu *= i;
+    }
+    if (chu != 0)
+        ans /= chu;
+    ans %= MOD;
+    return ans;
 }
 
-int main() {
+ll power(ll a, ll b)
+{
+    ll ans = 1;
+    while (b)
+    {
+        if ((b & 1) != 0)
+        {
+            ans *= a;
+            ans %= MOD;
+        }
+        a *= a;
+        a %= MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+ll solve()
+{
+    return ckn(k, n) * power(a, n) * power(b, m) % MOD;
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> a >> b >> k >> n >> m;
 
-
-    solve();
-
+    cout << solve();
     return 0;
 }
