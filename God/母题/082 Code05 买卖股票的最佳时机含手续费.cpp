315176dd38ecc580f5af69af4d721605316d498a
xxx <1748701882@qq.com>
2026-07-07 09:17:58 +0800

    vault backup: 2026-07-07 09:17:58

--- God/母题/082 Code05 买卖股票的最佳时机含手续费.cpp
+++ God/母题/082 Code05 买卖股票的最佳时机含手续费.cpp
@@ -6,5 +6,13 @@ class Solution
 public:
     int maxProfit(vector<int> &prices, int fee)
     {
+        int prepare = -prices[0] - fee;
+        int done = 0;
+        for (int i = 1; i < prices.size();i++){
+            int lastdone = done;
+            done = max(lastdone, prepare + prices[i]);
+            prepare = max(prepare, lastdone - prices[i] - fee);
+        }
+        return done;
     }
 };
