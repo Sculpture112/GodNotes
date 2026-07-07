e7ccd514b4a34e1ec7735a063cb359436ae9ed65
xxx <1748701882@qq.com>
2026-07-07 17:13:50 +0800

    vault backup: 2026-07-07 17:13:50

--- God/Money_Buys_Less_Happiness_Now.cpp
+++ God/Money_Buys_Less_Happiness_Now.cpp
@@ -1,14 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-
-void solve() {
-    
+const int N = 200007;
+int n, x;
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> x;
+        priority_queue<int> pq;
+
+        ll sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            int c;
+            cin >> c;
+            ll avialiable = 1LL * (i - 1) * x;
+
+            sum += c;
+            pq.push(c);
+            if (sum > avialiable)
+            {
+                sum -= pq.top();
+                pq.pop();
+            }
+        }
+
+        cout << pq.size() << "\n";
+    }
 
     solve();
 
