b6278cd942135abb71c4e90480e8db1a450d20df
xxx <1748701882@qq.com>
2026-07-07 11:23:46 +0800

    vault backup: 2026-07-07 11:23:46

--- God/母题/082 Code07 DI序列的有效排列.cpp
+++ God/母题/082 Code07 DI序列的有效排列.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+const int mod = 1000000007;
 class Solution
 {
 public:
     int numPermsDISequence(string s)
     {
+        int n = s.size()+1;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int less = 0;less<=n;less++){
+            dp[n][less] = 1;
+        }
+
+        for (int i = n - 1; i >=0 ;i--){
+            if(i == 0||s[i-1]){
+
+            }
+        }
     }
 };
