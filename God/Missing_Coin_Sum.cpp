59603b92b45af9b2232e0fb6896ccf0ab7ad361c
xxx <1748701882@qq.com>
2026-07-07 17:56:14 +0800

    vault backup: 2026-07-07 17:56:14

--- God/Missing_Coin_Sum.cpp
+++ God/Missing_Coin_Sum.cpp
@@ -2,15 +2,27 @@
 using namespace std;
 #define ll long long
 
-void solve() {
-    
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
 
-    solve();
-
+    ll ans = 1;
+    for(int x : a){
+        if(ans<x){
+            break;
+        }
+        ans += x;
+    }
+    cout << ans;
     return 0;
 }
