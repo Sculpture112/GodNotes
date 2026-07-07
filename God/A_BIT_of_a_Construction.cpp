4f120c30dbebbfe4ed2ee8d0d4dae594b33482ec
xxx <1748701882@qq.com>
2026-07-07 19:39:59 +0800

    vault backup: 2026-07-07 19:39:59

--- God/A_BIT_of_a_Construction.cpp
+++ God/A_BIT_of_a_Construction.cpp
@@ -2,13 +2,40 @@
 using namespace std;
 #define ll long long
 
-void solve() {
-    
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        ll k;
+        cin >> n >> k;
+        ll x = 1;
+        if(n == 1){
+            cout << k << "\n";
+            continue;
+        }
+        while (x * 2 - 1 <= k)
+        {
+            x <<= 1;
+        }
+
+        ll first = x - 1;
+        ll second = k - first;
+
+        cout<<first<<" "<<second;
+        for (int i = 3; i <= n;i++){
+            cout << " " << 0;
+        }
+        cout << "\n";
+    }
 
     solve();
 
