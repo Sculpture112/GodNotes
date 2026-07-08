6c7c5366a47a93c1cc50da6ed15e803659fa3c54
xxx <1748701882@qq.com>
2026-07-08 09:11:08 +0800

    vault backup: 2026-07-08 09:11:08

--- God/CF_1364_C_Ehab_and_Prefix_MEXs.cpp
+++ God/CF_1364_C_Ehab_and_Prefix_MEXs.cpp
@@ -1,83 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-int main()
-{
+void solve() {
+    
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin >> n;
-
-    vector<int> a(n), b(n, -1);
-    vector<int> inA(n + 1, 0);
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-        inA[a[i]] = 1;
-    }
-
-    set<int> canUse;
-
-    for (int x = 0; x <= n; x++)
-    {
-        if (!inA[x])
-        {
-            canUse.insert(x);
-        }
-    }
-
-    int last = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] > last)
-        {
-            b[i] = last;
-            canUse.erase(last);
-            last = a[i];
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        if (b[i] == -1)
-        {
-            if (canUse.empty())
-            {
-                cout << -1 << '\n';
-                return 0;
-            }
-
-            b[i] = *canUse.begin();
-            canUse.erase(canUse.begin());
-        }
-    }
-
-    set<int> seen;
-    int mex = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        seen.insert(b[i]);
-
-        while (seen.count(mex))
-        {
-            mex++;
-        }
-
-        if (mex != a[i])
-        {
-            cout << -1 << '\n';
-            return 0;
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << b[i] << " \n"[i == n - 1];
-    }
+    solve();
 
     return 0;
 }
