ddee1fc4f511fe210d93c09e9c0f7ffbc930afe3
xxx <1748701882@qq.com>
2026-07-08 11:29:28 +0800

    vault backup: 2026-07-08 11:29:27

--- God/Multiples_of_Length.cpp
+++ God/Multiples_of_Length.cpp
@@ -9,8 +9,10 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    solve();
-
-    return 0;
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n;i++)
+        cin >> a[i];
+        return 0;
 }
