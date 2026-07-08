10e897ffbcce0daa9df4e9352706afdca686e248
xxx <1748701882@qq.com>
2026-07-08 11:47:03 +0800

    vault backup: 2026-07-08 11:47:02

--- God/Multiples_of_Length.cpp
+++ God/Multiples_of_Length.cpp
@@ -2,34 +2,45 @@
 using namespace std;
 #define ll long long
 
-void solve() {
-    
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n;
     cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n;i++)
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
         cin >> a[i];
-    if(n == 1){
+    if (n == 1)
+    {
         cout << 1 << " " << 1 << "\n";
+
         cout << -1 * a[0] << "\n";
+        cout << "1 1" << "\n";
+        cout << "0" << "\n";
+        cout << "1 1" << "\n";
+        cout << "0" << "\n";
+        return 0;
     }
 
     cout << "1 1\n";
+    cout << -1 * a[0] << "\n";
     cout << 2 << " " << n << "\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i < n; i++)
+    {
         cout << a[i] * (n - 1) << " ";
     }
     cout << "\n";
-    
-    for (int i = 0; i < n; i++) {
+
+    cout << 2 << " " << n << "\n";
+    for (int i = 1; i < n; i++)
+    {
         cout << -a[i] * n << " ";
     }
     cout << "\n";
     return 0;
-        
 }
