0317d1af297c8980e6fa8413a83fe13390c811e2
xxx <1748701882@qq.com>
2026-07-08 09:49:06 +0800

    vault backup: 2026-07-08 09:49:06

--- God/CF_1364_C_Ehab_and_Prefix_MEXs.cpp
+++ God/CF_1364_C_Ehab_and_Prefix_MEXs.cpp
@@ -2,15 +2,62 @@
 using namespace std;
 #define ll long long
 
-void solve() {
-    
-}
-
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n, -1);
+    vector<int> inA(n + 1);
 
-    solve();
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        inA[a[i]] = 1;
+    }
 
-    return 0;
+    set<int> canUse;
+    for (int i = 0; i < n; i++)
+    {
+        if (!inA[i])
+        {
+            canUse.insert(inA[i]);
+        }
+    }
+
+    int last = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > last)
+        {
+            b[i] = last;
+            canUse.erase(last);
+            last = a[i];
+        }
+    }
+
+    for (int i = 0; i < n;i++){
+        if(b[i]==-1){
+            b[i] = *canUse.begin();
+            canUse.erase(canUse.begin());
+        }
+    }
+
+    set<int> seen;
+    int mex = 0;
+    for (int i = 0; i < n;i++){
+        seen.insert(b[i]);
+        if(seen.count(mex)){
+            mex++;
+        }
+        if(mex != a[i]){
+            cout << -1 << "\n";
+        }
+    }
+
+    for (int i = 0; i < n;i++){
+        cout << b[i] << " \n"[i == n - 1];
+    }
+        return 0;
 }
