33e48aceeffe57d49b31379d7f315eee2c515da2
xxx <1748701882@qq.com>
2026-07-09 10:43:07 +0800

    vault backup: 2026-07-09 10:43:07

--- God/A_Another_Puzzle_from_Papyrus.cpp
+++ God/A_Another_Puzzle_from_Papyrus.cpp
@@ -9,7 +9,32 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        int n, c;
+        cin >> n >> c;
+        vector<int> a(n), b(n);
+        int suma = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            suma += a[i];
+        }
+        int sumb = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> b[i];
+            sumb += b[i];
+        }
 
+        if(sumb>suma){
+            cout << -1 << "\n";
+            continue;
+        }
+
+        
+
+        
+    }
     solve();
 
     return 0;
