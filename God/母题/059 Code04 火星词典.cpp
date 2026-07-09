a5b2b021ccc5cbaa82dea2a1a840d602d544ef93
xxx <1748701882@qq.com>
2026-07-09 20:25:46 +0800

    vault backup: 2026-07-09 20:25:46

--- God/母题/059 Code04 火星词典.cpp
+++ God/母题/059 Code04 火星词典.cpp
@@ -6,5 +6,67 @@ class Solution
 public:
     string alienOrder(vector<string> &words)
     {
+        vector<int> indegree(26);
+        for (auto word : words)
+        {
+            for (char c : word)
+            {
+                indegree[c - '0'] = 0;
+            }
+        }
+
+        vector<vector<int>> graph(words.size());
+        for (int i = 0, j; i < words.size() - 1; i++)
+        {
+            string cur = words[i];
+            string nxt = words[i + 1];
+
+            j = 0;
+            int len = min(cur.size(), nxt.size());
+            for (; j < len; j++)
+            {
+                if (cur[j] != nxt[j])
+                {
+                    graph[cur[j]].push_back(nxt[j]);
+                    indegree[nxt[j]]++;
+                    break;
+                }
+            }
+            if (j < cur.size() && j == nxt.size())
+            {
+                return "";
+            }
+        }
+
+        vector<int> queue(26);
+
+        int l = 0, r = 0;
+        int kind = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            if (indegree[i] == 0)
+            {
+                queue[r++] = i;
+            }
+            if (indegree[i] != -1)
+            {
+                kind++;
+            }
+        }
+
+        string ans;
+        while (l < r)
+        {
+            int cur = queue[l++];
+            ans.push_back(cur + '0');
+            for (int nxt : graph[cur])
+            {
+                if (--indegree[nxt] == 0)
+                {
+                    queue[r++] = nxt;
+                }
+            }
+        }
+        return ans.size() == kind ? ans : "";
     }
 };
