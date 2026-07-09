c21a337327282494fc4ab64bd507a97067a8c64e
xxx <1748701882@qq.com>
2026-07-09 19:47:18 +0800

    vault backup: 2026-07-09 19:47:18

--- God/母题/059 Code02 拓扑排序模版动态建图牛客.cpp
+++ God/母题/059 Code02 拓扑排序模版动态建图牛客.cpp
@@ -1,11 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> indegree(n + 1, 0);
+    vector<vector<int>> graph(n + 1);
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        indegree[v]++;
+    }
+    int l = 0, r = 0;
+    vector<int> queue(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (indegree[i] == 0)
+        {
+            queue[r++] = i;
+        }
+    }
+    int cnt = 0;
+
+    while (l < r)
+    {
+        int cur = queue[l++];
+        cnt++;
+        for (int nxt : graph[cur])
+        {
+            if (--indegree[nxt] == 0)
+            {
+                queue[r++] = nxt;
+            }
+        }
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << queue[i] << " ";
+    }
+    cout << queue[n - 1];
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
