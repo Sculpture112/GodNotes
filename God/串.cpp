d34fe94cdaee958619cd06e8c95570d6fd342bd2
xxx <1748701882@qq.com>
2026-07-09 10:09:13 +0800

    vault backup: 2026-07-09 10:09:13

--- God/串.cpp
+++ God/串.cpp
@@ -9,7 +9,33 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        string s;
+        cin >> n >> s;
 
+        int g = gcd(n, 9);
+
+        vector<vector<int>> cnt(g, vector<int>(9, 0));
+        vector<int> sz(g, 0);
+        for (int i = 0; i < n;i++){
+            int group = i % 9;
+            int r = (s[i] - '0') % 9;
+            cnt[group][r]++;
+            sz[g]++;
+        }
+
+        vector<vector<int>> cost(g, vector<int>(9, 0));
+        for (int i = 0; i < g;i++){
+            for (int j = 0; j < 9;j++){
+                cost[i][j] = sz[i] - cnt[i][j];
+            }
+        }
+
+        
+    }
     solve();
 
     return 0;
