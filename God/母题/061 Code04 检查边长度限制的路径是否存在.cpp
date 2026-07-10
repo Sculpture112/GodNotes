bcd84dc7a1dcc7566ad0ebe34acc2ddd6ab48ecc
xxx <1748701882@qq.com>
2026-07-10 15:19:29 +0800

    vault backup: 2026-07-10 15:19:29

--- God/母题/061 Code04 检查边长度限制的路径是否存在.cpp
+++ God/母题/061 Code04 检查边长度限制的路径是否存在.cpp
@@ -6,5 +6,9 @@ class Solution
 public:
     vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>> &edgeList, vector<vector<int>> &queries)
     {
+        int n = edgeList.size();
+        int m = queries.size();
+
+        sort(edgeList.begin(),edgeList.end(),[](auto & a,))
     }
 };
