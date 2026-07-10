623fb42362c018aaa94ed6b68cd696daade6b415
xxx <1748701882@qq.com>
2026-07-10 17:51:18 +0800

    vault backup: 2026-07-10 17:51:18

--- God/P_1113_USACO_02_FEB_杂务.cpp
+++ God/P_1113_USACO_02_FEB_杂务.cpp
@@ -7,18 +7,54 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
+const int MAXN = 100001;
+vector<vector<pair<int,int>>> graph(MAXN);
+int indegree[MAXN];
+void solve()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
 
-void solve() {
+        int u;
+        int w;
+        cin >> u >> w;
+        int x;
+        cin >> x;
 
+        while (x > 0)
+        {
+            graph[x].push_back({w,x});
+            indegree[u]++;
+            cin >> x;
+        }
+
+        vector<pair<int, int>> queue;
+        int l = 0, r = 0;
+        for (int i = 1; i <= n;i++){
+            if(indegree[i] == 0)
+            {
+                queue[r++] = {};
+            }
+        }
+
+        while(l<r){
+            int cur = queue[l++];
+
+        }
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
