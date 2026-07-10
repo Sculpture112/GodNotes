8ea0626d9f0ca4c4ec2b5635b6186f43c861b72e
xxx <1748701882@qq.com>
2026-07-10 10:24:18 +0800

    vault backup: 2026-07-10 10:24:18

--- God/母题/059 Code05 戳印序列.cpp
+++ God/母题/059 Code05 戳印序列.cpp
@@ -1,15 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    
-}
+class Solution
+{
+public:
+    vector<int> movesToStamp(string stamp, string target)
+    {
+        int n = target.size();
+        int m = stamp.size();
+        string s = stamp;
+        string t = target;
+        vector<int> indegree(n - m + 1, m);
+        vector<vector<int>> graph(n);
+        // graph[x] ： x 可以解锁的stamp的位置
+        vector<int> queue(n - m + 1, 0);
+        int l = 0, r = 0;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+        for (int i = 0; i <= n - m; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (s[i + j] == s[j])
+                {
+                    if (--indegree[i] == 0)
+                    {
+                        queue[r++] = i;
+                    }
+                }
+                else
+                {
+                    graph[i + j].push_back(i);
+                }
+            }
+        }
 
-    solve();
-
-    return 0;
-}
+        vector<int> path;
+        while (l < r)
+        {
+            int cur = queue[l++];
+            path.push_back(cur);
+            
+        }
+    }
+};
