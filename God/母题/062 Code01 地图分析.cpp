f6c1732d86838157c64262c5b7decf03053844ac
xxx <1748701882@qq.com>
2026-07-10 17:10:31 +0800

    vault backup: 2026-07-10 17:10:31

--- God/母题/062 Code01 地图分析.cpp
+++ God/母题/062 Code01 地图分析.cpp
@@ -53,7 +53,7 @@ public:
                 {
                     int nx = x + dx[i];
                     int ny = y + dy[i];
-                    if (nx < 0 || nx == n || ny < 0 || ny == m || !visited[nx][ny])
+                    if (nx < 0 || nx == n || ny < 0 || ny == m || visited[nx][ny])
                     {
                         continue;
                     }
