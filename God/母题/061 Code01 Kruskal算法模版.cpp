9e60cddc732a672746d14d0a4b16d5a9b513142f
xxx <1748701882@qq.com>
2026-07-10 11:47:57 +0800

    vault backup: 2026-07-10 11:47:57

--- God/母题/061 Code01 Kruskal算法模版.cpp
+++ God/母题/061 Code01 Kruskal算法模版.cpp
@@ -1,11 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MAXN = 5001;
+const int MAXM = 20005;
+int father[MAXN];
 
-void solve() {
-    
+int find(int i)
+{
+
+    if (i != father[i])
+    {
+        father[i] = find(father[i]);
+    }
+    return father[i];
+}
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n;i++){
+        father[i] = i;
+    }
+
+    int edges[MAXM][3];
+    for (int i = 0; i < m;i++){
+        cin >> edges[i][0] >> edges[i][1] >> edges[i][2];
+    }
+
+    sort(edges,edges+m,[])
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
