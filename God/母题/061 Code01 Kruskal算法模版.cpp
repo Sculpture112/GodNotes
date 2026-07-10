69ededdcc6e13a276602196992ab3f3a4219519e
xxx <1748701882@qq.com>
2026-07-10 11:54:23 +0800

    vault backup: 2026-07-10 11:54:23

--- God/母题/061 Code01 Kruskal算法模版.cpp
+++ God/母题/061 Code01 Kruskal算法模版.cpp
@@ -13,21 +13,50 @@ int find(int i)
     }
     return father[i];
 }
+bool unite(int x,int y){
+    int fx = find(x);
+    int fy = find(y);
+    if(fx != fy){
+        father[fx] = fy;
+        return true;
+    }
+    return false;
+}
 void solve()
 {
     int n, m;
     cin >> n >> m;
 
-    for (int i = 1; i <= n;i++){
+    for (int i = 1; i <= n; i++)
+    {
         father[i] = i;
     }
 
     int edges[MAXM][3];
-    for (int i = 0; i < m;i++){
+    for (int i = 0; i < m; i++)
+    {
         cin >> edges[i][0] >> edges[i][1] >> edges[i][2];
     }
 
-    sort(edges,edges+m,[])
+    sort(edges, edges + m, [](const auto &a, const auto &b)
+         { return a[2] < b[2]; });
+
+    int ans = 0;
+    int edgecnt = 0;
+    for (int i = 0; i < m;i++){
+        if(unite(edges[i][0],edges[i][1])){
+            edgecnt++;
+            ans += edges[i][2];
+        }
+    }
+
+    if(edgecnt == n-1){
+        cout << ans;
+    }
+    else{
+        cout << "orz";
+    }
+    return;
 }
 
 int main()
