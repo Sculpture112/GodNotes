902879bf5eb41db0f5129bc82f5ba2d51f2058a7
xxx <1748701882@qq.com>
2026-07-10 14:37:29 +0800

    vault backup: 2026-07-10 14:37:29

--- God/母题/061 Code02 Prim算法模版动态空间.cpp
+++ God/母题/061 Code02 Prim算法模版动态空间.cpp
@@ -1,12 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
+void solve()
+{
     int n, m;
     cin >> n >> m;
+    vector<vector<pair<int, int>>> graph(n + 1);
+    for (int i = 0, u, v, w; i < n; i++)
+    {
+        cin >> u >> v >> w;
+        graph[u].push_back({w, v});
+        graph[v].push_back({w, u});
+    }
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+
+    for(auto edge:graph[1]){
+        heap.push({edge.first, edge.second});
+    }
+
+    vector<bool> set(n + 1, false);
+    set[1] = true;
+    int nodeCnt = 1;
+    int ans = 0;
+    while(!heap.empty()){
+        auto [cost, nxt] = heap.top();
+        heap.pop();
+
+
+        if(!set[nxt])
+        {
+            nodeCnt++;
+            ans += cost;
+            set[nxt] = true;
+            for (auto e : graph[nxt])
+            {
+                heap.push({e.first, e.second});
+            }
+        }
+    }
+    if(nodeCnt == n){
+        cout << ans;
+    }
+    else{
+        cout << "orz";
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
