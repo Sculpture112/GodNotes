a7e02387c481d61c478df03011a9cbaa2ea156a4
xxx <1748701882@qq.com>
2026-07-10 15:20:36 +0800

    vault backup: 2026-07-10 15:20:36

--- God/母题/061 Code04 检查边长度限制的路径是否存在.cpp
+++ God/母题/061 Code04 检查边长度限制的路径是否存在.cpp
@@ -9,6 +9,12 @@ public:
         int n = edgeList.size();
         int m = queries.size();
 
-        sort(edgeList.begin(),edgeList.end(),[](auto & a,))
+        sort(edgeList.begin(), edgeList.end(), [](auto &a, auto &b)
+             { return a[2] < b[2]; });
+
+        vector<vector<int>> queries(m + 1, 4);
+        for (int i = 0; i < m;i++){
+            queries[i][0] = queries
+        }
     }
 };
