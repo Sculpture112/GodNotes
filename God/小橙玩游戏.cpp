9cc61955ddfb80e5f0db980643f488da36c62381
xxx <1748701882@qq.com>
2026-07-11 10:59:51 +0800

    vault backup: 2026-07-11 10:59:51

--- God/小橙玩游戏.cpp
+++ God/小橙玩游戏.cpp
@@ -7,26 +7,32 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
-bool f(int x){
-    if(x<=0){
+bool F = true;
+bool f(int x)
+{
+    if (x <= 0)
+    {
         return false;
+
     }
-    
+    x -= F?
 }
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
-    
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
     cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
