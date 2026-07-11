f3c7e5b0187091d2f0288f34ab7f296a9f42deca
xxx <1748701882@qq.com>
2026-07-11 13:54:47 +0800

    vault backup: 2026-07-11 13:54:47

--- God/1_s_For_All.cpp
+++ God/1_s_For_All.cpp
@@ -8,27 +8,40 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-int sum(int x){
+int sum(int x)
+{
     int ans = 0;
-    while(x!=0){
-        ans += x % 10;
+    while (x != 0)
+    {
+        int u = x % 10;
+        if (u == 8)
+        {
+            ans += 6;
+        }
+        else if (u == 9)
+        {
+            ans += 6;
+        }
+        else if(u == 0)
         x /= 10;
     }
     return ans;
 }
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
-       
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
