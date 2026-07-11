dc73c94fc3f133d4c597a3257913a57f8330a8ae
xxx <1748701882@qq.com>
2026-07-11 10:06:46 +0800

    vault backup: 2026-07-11 10:06:46

--- God/母题/064 Code01 Dijkstra算法模版（Leetcode）.cpp
+++ God/母题/064 Code01 Dijkstra算法模版（Leetcode）.cpp
@@ -13,5 +13,39 @@ class Solution
 public:
     int networkDelayTime(vector<vector<int>> &times, int n, int k)
     {
+        vector<vector<pair<int, int>>> graph(n + 1);
+
+        vector<int> distance(n + 1, INF);
+        vector<bool> visited(n + 1, false);
+        distance[k] = 0;
+
+        priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, k});
+
+        while(!pq.empty()){
+            auto [d, u] = pq.top();
+            pq.pop();
+            if(visited[u]){
+                continue;
+            }
+            visited[u] = true;
+            for (auto [w,v] : graph[u])
+            {
+                
+                if(!visited[v] && distance[u] + w < distance[v]){
+                    distance[v] = distance[u] + w;
+                    pq.push({distance[v], v});
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= n;i++){
+            if(distance[i] == -1){
+                return -1;
+            }
+            ans = max(ans, distance[i]);
+        }
+        return ans;
     }
 };
