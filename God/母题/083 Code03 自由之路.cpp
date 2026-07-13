3e3aae87d94b297863ef985ae0e71eb1b68d4f32
xxx <1748701882@qq.com>
2026-07-13 15:19:20 +0800

    vault backup: 2026-07-13 15:19:20

--- God/母题/083 Code03 自由之路.cpp
+++ God/母题/083 Code03 自由之路.cpp
@@ -11,8 +11,47 @@ const ll LINF = 4e18;
 class Solution
 {
 public:
+    int n, m;
+    vector<vector<int>> dp;
+    vector<vector<int>> where;
+    string r, k;
     int findRotateSteps(string ring, string key)
     {
-        
+        n = ring.size();
+        m = key.size();
+        r = ring;
+        k = key;
+
+        where.assign(26, {});
+        for (int i = 0; i < n; i++)
+        {
+            where[ring[i] - 'a'].push_back(i);
+        }
+
+        dp.assign(n, vector<int>(m, -1));
+
+    }
+    int f(int i ,int j){
+        if(j == m){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(r[i] == k[j]){
+            return dp[i][j] = 1 + f(i,j+1);
+        }
+
+        int jump1;
+    }
+    int clock(int i,int v){
+        auto &arr = where[v];
+        auto it = upper_bound(arr.begin(), arr.end(), i);
+
+        return it == arr.end()?arr[0]:*it;
+    }
+    int revclock(int i ,int v){
+        auto &arr = where[v];
+        auto it = 
     }
 };
