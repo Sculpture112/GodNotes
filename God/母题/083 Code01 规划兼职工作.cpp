a75229f6f8faf258057b7db61a98f52e37caada3
xxx <1748701882@qq.com>
2026-07-13 11:17:29 +0800

    vault backup: 2026-07-13 11:17:29

--- God/母题/083 Code01 规划兼职工作.cpp
+++ God/母题/083 Code01 规划兼职工作.cpp
@@ -13,5 +13,42 @@ class Solution
 public:
     int jobScheduling(vector<int> &startTime, vector<int> &endTime, vector<int> &profit)
     {
+        int n = startTime.size();
+
+        vector<array<int, 3>> jobs(n);
+        for (int i = 0; i < n; i++)
+        {
+            jobs[i][0] = startTime[i];
+            jobs[i][1] = endTime[i];
+            jobs[i][2] = profit[i];
+        }
+
+        vector<int> dp(n);
+        int dp[0] = jobs[0][2];
+        for (int i = 0; i < n; i++)
+        {
+            
+        }
+
+        int find(vector<int> & jobs, int r, int start)
+        {
+            int ans = -1;
+            int l = 0;
+            while (l <= r)
+            {
+                int m = (l - r) / 2 + r / 2;
+
+                if (jobs[m][1] < start)
+                {
+                    ans = m;
+                    l = m + 1;
+                }
+                else
+                {
+                    r = m - 1;
+                }
+            }
+            return ans;
+        }
     }
 };
