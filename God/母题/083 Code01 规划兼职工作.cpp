aae2164c02b21335a60c78278d21f7fc8c7d5688
xxx <1748701882@qq.com>
2026-07-13 11:25:13 +0800

    vault backup: 2026-07-13 11:25:13

--- God/母题/083 Code01 规划兼职工作.cpp
+++ God/母题/083 Code01 规划兼职工作.cpp
@@ -22,38 +22,40 @@ public:
             jobs[i][1] = endTime[i];
             jobs[i][2] = profit[i];
         }
-        sort(jobs.begin(), jobs.end(), [](const auto &a, const auto &b) { return a[1] < b[1]; });
+        sort(jobs.begin(), jobs.end(), [](const auto &a, const auto &b)
+             { return a[1] < b[1]; });
         vector<int> dp(n);
         dp[0] = jobs[0][2];
         for (int i = 1; i < n; i++)
         {
             int cur = jobs[i][2];
             int pre = find(jobs, i - 1, jobs[i][0]);
-            if(pre !=-1){
+            if (pre != -1)
+            {
                 cur += dp[pre];
             }
-            dp[n] = max(dp[n - 1], cur);
+            dp[i] = max(dp[i - 1], cur);
         }
-
-        int find(vector<int> & jobs, int r, int start)
+        return dp[n - 1];
+    }
+    int find(vector<array<int,3>> &jobs, int r, int start)
+    {
+        int ans = -1;
+        int l = 0;
+        while (l <= r)
         {
-            int ans = -1;
-            int l = 0;
-            while (l <= r)
-            {
-                int m = (l - r) / 2 + r / 2;
+            int m = (l - r) / 2 + r / 2;
 
-                if (jobs[m][1] < start)
-                {
-                    ans = m;
-                    l = m + 1;
-                }
-                else
-                {
-                    r = m - 1;
-                }
+            if (jobs[m][1] < start)
+            {
+                ans = m;
+                l = m + 1;
             }
-            return ans;
+            else
+            {
+                r = m - 1;
+            }
         }
+        return ans;
     }
 };
