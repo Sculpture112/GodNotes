26aa7f42e19d3801016f1523111a276cf7998553
xxx <1748701882@qq.com>
2026-07-13 16:08:52 +0800

    vault backup: 2026-07-13 16:08:52

--- God/母题/083 Code04 累加和不大于k的最长子数组.cpp
+++ God/母题/083 Code04 累加和不大于k的最长子数组.cpp
@@ -8,8 +8,48 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+int solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
+    vector<int> minisum(n);
+    vector<int> minisumend(n);//这段和是以谁结束
+    minisum[n - 1] = arr[n - 1];
+    minisumend[n - 1] = n - 1;
+    for (int i = n - 2; i >= 0;i--){
+        if(minisum[i+1] <0){
+            minisum[i] = minisum[i + 1] + arr[i];
+            minisumend[i] = minisumend[i + 1];
+        }
+        else{
+            minisum[i] = arr[i];
+            minisumend[i] = i;
+        }
+    }
+
+    int end = 0;
+    int sum = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        while(end <n && sum + minisum[end] <=k){
+            sum += minisum[end];
+            end = minisumend[end] + 1;
+        }
+
+        if(end>i){
+            ans = max(ans, end - i);
+            sum -= arr[i];
+        }
+        else{
+            end = i + 1;
+        }
+    }
+    return ans;
 }
 
 int main() {
@@ -18,7 +58,7 @@ int main() {
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
-
+    while (T--) cout<<solve();
+    
     return 0;
 }
