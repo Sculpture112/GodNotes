911d729d3a2a001624aaf827843d8a5b3a2137ec
xxx <1748701882@qq.com>
2026-07-13 20:38:29 +0800

    vault backup: 2026-07-13 20:38:28

--- God/小橙访问传送门.cpp
+++ God/小橙访问传送门.cpp
@@ -9,7 +9,34 @@ const ll LINF = 4e18;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> r(n),b(m);
+    int y = INF, x = INF;
+    for (int i = 0; i < n; i++) {
+        cin >> r[i];
+        x = min(x, abs(r[i]));
+    }
+    int mn = INF;
+    sort(r.begin(), r.end());
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+        auto it = lower_bound(r.begin(), r.end(), b[i]);
+        if (it != r.end())
+        {
+            mn = min(mn, abs(*it - b[i]));
+        }
 
+        if (it != r.begin())
+        {
+            --it;
+            mn = min(mn, abs(*it - b[i]));
+        }
+        y = min(y, abs(b[i]));
+        
+    }
+
+    cout << x + y + mn;
 }
 
 int main() {
