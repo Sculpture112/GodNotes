de0c26d8119c48516737720b6613e211b2bcac7b
xxx <1748701882@qq.com>
2026-07-15 17:00:22 +0800

    vault backup: 2026-07-15 17:00:22

--- God/母题/084 Code04 完全没有重复的数字个数.cpp
+++ God/母题/084 Code04 完全没有重复的数字个数.cpp
@@ -13,6 +13,54 @@ class Solution
 public:
     int countSpecialNumbers(int n)
     {
-        
+        int len = 1;
+        int offset = 1;
+        int tmp = n / 10;
+        while (tmp > 0)
+        {
+            offset *= 10;
+            len++;
+            tmp /= 10;
+        }
+
+        vector<int> cnt(len, 0);
+        cnt[0] = 1;
+        for (int i = 1, k = 10 - len + 1; i < len; k++, i++)
+        {
+            cnt[i] = cnt[i - 1] * k;
+        }
+
+        int ans = 9;
+        if (len >= 2)
+        {
+            ans = 9;
+            for (int i = 0, a = 9, b = 9; i < n; i++, b--)
+            {
+                a *= b;
+                ans += a;
+            }
+        }
+
+        int first = n / offset;
+        ans += (first - 1) * cnt[len - 1];
+
+        ans += f(cnt, len, offset, n, 1 << first);
+        return ans;
+    }
+    int f(vector<int>& cnt,int len ,int offset,int n,int status){
+        if(len == 0)
+            return 1;
+
+        int ans = 0;
+        int first = n / offset % 10;
+        for (int i = 0; i < first; i++) {
+            if((1<<i)&status ==0){
+                ans += cnt[len - 1];
+            }
+        }
+        if((1<<first)&status == 0){
+            ans += f(cnt, len - 1, offset, n, status | (1 << first));
+        }
+        return ans;
     }
 };
