d20440adec82b08e2b34f5b2bf3b1c33f990640c
xxx <1748701882@qq.com>
2026-07-15 12:11:26 +0800

    vault backup: 2026-07-15 12:11:26

--- God/母题/084 Code03 统计整数数目.cpp
+++ God/母题/084 Code03 统计整数数目.cpp
@@ -8,17 +8,79 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+class Solution
+{
+public:
+    const int MOD = 1e9 + 7;
+    string num;
+    int dp[23][401][2];
+    int minsum, maxsum, len;
 
-}
+    void build()
+    {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < maxsum; j++) {
+                dp[i][j][0] = -1;
+                dp[i][j][1] = -1;
+            }
+        }
+    }
+    
+    int count(string num1, string num2, int min_sum, int max_sum)
+    {
+        minsum = min_sum;
+        maxsum = max_sum;
+        num = num2;
+        len = num.size();
+        build();
+        int ans = f(0, 0, 0);
+        num = num2;
+        len = num.size();
+        build();
+        ans = (ans + f(0, 0, 0)) % MOD;
+        if(check()){
+            ans = (ans + 1) % MOD;
+        }
+        return ans;
+    }
+    int f(int sum,int free,int i){
+        if(sum>maxsum){
+            return 0;
+        }
+        if(sum + (len - i)*9<minsum){
+            return 0;
+        }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+        if(i == len)
+            return 1;
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
+        if(dp[i][sum][free] != -1){
+            return dp[i][sum][free];
+        }
 
-    return 0;
-}
+        int cur = num[i]-'0';
+        int ans = 0;
+        if(free == 0){
+            for (int pick = 0;pick<cur;pick++){
+                ans += (ans + f(sum + pick, 1, i + 1))%MOD;
+                
+            }
+            ans += (ans +f(sum + cur, 0, i + 1))%MOD;
+        }
+        else{
+            for (int pick = 0; pick <= 9;pick++){
+                ans += (ans + f(sum + pick, 1, i + 1)) % MOD;
+            }
+        }
+        dp[i][sum][free] = ans;
+        return ans;
+    }
+    bool check(){
+        int sum = 0;
+
+        for(char cha:num){
+            sum += cha - '0';
+        }
+        return sum <= maxsum && sum >= minsum;
+    }
+};
