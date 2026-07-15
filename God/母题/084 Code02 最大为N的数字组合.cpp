5d8221eff8c09af535b206f6261ce005ad921953
xxx <1748701882@qq.com>
2026-07-15 11:13:39 +0800

    vault backup: 2026-07-15 11:13:39

--- God/母题/084 Code02 最大为N的数字组合.cpp
+++ God/母题/084 Code02 最大为N的数字组合.cpp
@@ -13,5 +13,51 @@ class Solution
 public:
     int atMostNGivenDigitSet(vector<string> &digits, int n)
     {
+        int len = 1;
+        int offset = 1;
+        int tmp = n / 10;
+        while(tmp>0){
+            tmp /= 10;
+            offset *= 10;
+            len++;
+        }
+
+        vector<int> digit(digits.size());
+        for (int i = 0; i < digits.size(); i++) {
+            digit[i] = stoi(digits[i]);
+        }
+
+        return f(digit, len, n, offset, 0, 0);
+    }
+
+    int f(vector<int>& digit,int len,int num,int offset,int free,int fix)
+    {
+        if(len == 0){
+            return fix == 1 ? 1 : 0;
+        }
+        int ans = 0;
+        int cur = num / offset % 10;
+        if (fix == 0)
+        {
+            ans += f(digit, len-1, num, offset / 10, 1, 0);
+        }
+
+        if(free ==0){
+            for(int x:digit){
+
+                if(x<cur){
+                    ans += f(digit, len-1, num, offset / 10, 1, 1);
+                }
+                else if(x==cur){
+                    ans += f(digit, len - 1, num, offset / 10, 0, 1);
+                }
+                else
+                    break;
+            }
+        }
+        else{
+            ans += digit.size() * f(digit, len - 1, num, offset / 10, 1, 1);
+        }
+        return ans;
     }
 };
