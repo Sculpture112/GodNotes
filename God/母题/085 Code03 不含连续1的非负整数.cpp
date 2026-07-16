ab020c382d5589e5a78e203b58cfc3377af5af42
xxx <1748701882@qq.com>
2026-07-16 11:25:30 +0800

    vault backup: 2026-07-16 11:25:30

--- God/母题/085 Code03 不含连续1的非负整数.cpp
+++ God/母题/085 Code03 不含连续1的非负整数.cpp
@@ -13,6 +13,29 @@ class Solution
 public:
     int findIntegers(int n)
     {
-        
+        int cnt[31];
+        cnt[0] = 1;
+        cnt[1] = 2;
+        for (int i = 2; i <= 30; i++) {
+            cnt[i] = cnt[i - 1] + cnt[i - 2];
+        }
+
+        return f(cnt, n, 30);
+    }
+    int f(int cnt[],int num,int i){
+        if(i == -1){
+            return 1;
+        }
+        int ans = 0;
+        if((num&(1<<i)) != 0){
+            ans += f(cnt, num, i - 1);
+
+            if((num&(1<<(i+1)))!=0)
+            {
+                return ans;
+            }
+        }
+        ans += f(cnt, num, i - 1);
+        return ans;
     }
 };
