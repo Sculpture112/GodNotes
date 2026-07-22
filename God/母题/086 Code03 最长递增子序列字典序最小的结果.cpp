ed14e19e24131643a35f6180bebed1ef96180e4b
xxx <1748701882@qq.com>
2026-07-22 17:18:37 +0800

    vault backup: 2026-07-22 17:18:37

--- God/母题/086 Code03 最长递增子序列字典序最小的结果.cpp
+++ God/母题/086 Code03 最长递增子序列字典序最小的结果.cpp
@@ -10,31 +10,38 @@ int nums[MAXN], dp[MAXN], endsArr[MAXN], ans[MAXN];
 int n, k;
 #define all(x) (x).begin(), (x).end()
 
-int bs(int len,int num){
-    int l = 0, r = len - 1, ans = -1,m;
-    while(l<=r){
-        m = (l - r) / 2 + r;
-        if(endsArr[m] <= num){
+int bs(int len, int num)
+{
+    int l = 0, r = len - 1, ans = -1, m;
+    while (l <= r)
+    {
+        m = l + (r - l) / 2;
+        if (endsArr[m] <= num)
+        {
             ans = m;
             r = m - 1;
         }
-        else{
+        else
+        {
             l = m + 1;
         }
     }
     return ans;
 }
 
-int filldp(){
+int filldp()
+{
     int len = 0;
     for (int i = n - 1, find; i >= 0; i--)
     {
         find = bs(len, nums[i]);
-        if(find == -1){
+        if (find == -1)
+        {
             endsArr[len++] = nums[i];
             dp[i] = len;
         }
-        else{
+        else
+        {
             endsArr[find] = nums[i];
             dp[i] = find + 1;
         }
@@ -42,31 +49,38 @@ int filldp(){
     return len;
 }
 
-void lis(){
+void lis()
+{
     k = filldp();
     fill(ans, ans + k, INF);
-    for (int i = 0; i < n; i++) {
-        if(dp[i] = k){
+    for (int i = 0; i < n; i++)
+    {
+        if (dp[i] == k)
+        {
             ans[0] = nums[i];
         }
-        else if(ans[k-dp[i]-1]<nums[i]){
+        else if (ans[k - dp[i] - 1] < nums[i])
+        {
             ans[k - dp[i]] = nums[i];
         }
     }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     // cin >> T;
-
-    for (int i = 0; i < n; i++) {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
         cin >> nums[i];
     }
 
     lis();
-    for (int i = 0; i < k-1; i++) {
+    for (int i = 0; i < k - 1; i++)
+    {
         cout << ans[i] << " ";
     }
     cout << ans[k - 1];
