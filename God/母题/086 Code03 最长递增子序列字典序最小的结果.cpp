ca760aaaccb548dd5b67d99ea025ff2814eaf2ee
xxx <1748701882@qq.com>
2026-07-22 17:00:55 +0800

    vault backup: 2026-07-22 17:00:55

--- God/母题/086 Code03 最长递增子序列字典序最小的结果.cpp
+++ God/母题/086 Code03 最长递增子序列字典序最小的结果.cpp
@@ -5,11 +5,29 @@ using ll = long long;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
-
+const int MAXN = 100001;
+int nums[MAXN], dp[MAXN], endsArr[MAXN], ans[MAXN];
+int n, k;
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+int bs(int len,int num){
+    int l = 0, r = len - 1, ans = -1,m;
+    while(l<=r){
+        m = (l - r) / 2 + r;
+        if(endsArr[m] <= num){
+            ans = m;
+            r = m - 1;
+        }
+        else{
+            l = m + 1;
+        }
+    }
+    return ans;
+}
 
+int filldp(){
+    int len = 0;
+    
 }
 
 int main() {
@@ -19,6 +37,12 @@ int main() {
     int T = 1;
     // cin >> T;
     while (T--) solve();
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+
 
     return 0;
 }
