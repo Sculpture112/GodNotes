bb2d5c51b5de16aa3af6c4375f2ed89d4858ee5a
xxx <1748701882@qq.com>
2026-07-22 12:49:19 +0800

    vault backup: 2026-07-22 12:49:19

--- God/母题/086 Code01 最长公共子序列其中一个结果.cpp
+++ God/母题/086 Code01 最长公共子序列其中一个结果.cpp
@@ -8,18 +8,62 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 const int MAXN = 5001;
+int dp[MAXN][MAXN];
+char ans[MAXN];
+int n, m, k;
+string s1, s2;
 
-void solve() {
-
+void filldp()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
 }
 
-int main() {
+void lcs()
+{
+    k = dp[n][m];
+    for (int len = k, i = n, j = m; len > 0;)
+    {
+        if (s1[i-1] == s2[j-1])
+        {
+            ans[--len] = s1[i-1];
+            i--;
+            j--;
+        }
+        else if (dp[i - 1][j] >= dp[i][j - 1])
+        {
+            i--;
+        }
+        else
+        {
+            j--;
+        }
+    }
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    cin >> s1 >> s2;
+    n = s1.size();
+    m = s2.size();
+    filldp();
+    lcs();
+    if (k == 0)
+    {
+        cout << -1;
+    }
+    else
+        cout.write(ans, k), cout << "\n";
 
     return 0;
 }
