2b7469f9d02c88bfbd21e660c88b95a54bff7bf6
xxx <1748701882@qq.com>
2026-07-24 14:19:54 +0800

    vault backup: 2026-07-24 14:19:54

--- God/母题/087 Code01 贿赂怪兽.cpp
+++ God/母题/087 Code01 贿赂怪兽.cpp
@@ -8,17 +8,32 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+int solve(int n,vector<int>& a,vector<int>& b) {
+    int sum = accumulate(b.begin(), b.end(),0);
 
+    vector<vector<int>> dp(n + 1, vector<int>(sum + 1, ));
+
+    for (int i = 1; i <= n;i++){
+        for (int j = sum; j >= 0;j--){
+            if(dp[i-1][j] > a[i]){
+                dp[i][j] = dp[i - 1][j];
+            }
+
+            if(j>b[i] && )
+        }
+    }
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
-
+    int n;
+    cin >> n;
+    vector<int> a(n + 1), b(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i] >> b[i];
+    }
     return 0;
 }
