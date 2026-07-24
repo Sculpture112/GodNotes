be10e7636daa1beeb164e8aedf2140fe76f9f982
xxx <1748701882@qq.com>
2026-07-24 16:33:51 +0800

    vault backup: 2026-07-24 16:33:51

--- God/母题/087 Code02 选择k个数字使得两集合累加和相差不超过1.cpp
+++ God/母题/087 Code02 选择k个数字使得两集合累加和相差不超过1.cpp
@@ -21,3 +21,25 @@ vector<int> pick(int n,int k){
     return ans;
 }
 
+vector<int> generate(ll sum,int n,int k){
+    ll minsum = 1LL * (k + 1) * k / 2;
+    ll need = sum - minsum;
+    ll range = n - k;
+    if(minsum>sum || sum > minsum + range * k){
+        return {};
+    }
+
+    ll rightsz = need / range;
+    ll midindex = k - rightsz + need % range;
+    ll leftsz = k - rightsz - (need % range == 0 ? 0 : 1);
+
+    vector<int> ans;
+    for (int i = 0; i < leftsz;i++){
+        ans[i] = i + 1;
+    }
+    ans[leftsz] = midindex;
+    for (int i = k - 1, j = 0; j < rightsz;j++,i--){
+        ans[i] = n - j;
+    }
+    return ans;
+}
