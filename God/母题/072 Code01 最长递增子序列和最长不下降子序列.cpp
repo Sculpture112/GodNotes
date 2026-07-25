3ccf7e26e5f08a8bf48dd536ad7bb639602efa2a
xxx <1748701882@qq.com>
2026-07-25 10:43:13 +0800

    vault backup: 2026-07-25 10:43:12

--- God/母题/072 Code01 最长递增子序列和最长不下降子序列.cpp
+++ God/母题/072 Code01 最长递增子序列和最长不下降子序列.cpp
@@ -8,17 +8,46 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        if(n==0)
+            return 0;
 
-}
+        vector<int> end(n);
+        int len = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int find = bs(end, nums[i], len);
+            if(find ==-1){
+                end[len++] = nums[i];
+            }
+            else{
+                end[find] = nums[i];
+            }
+        }
+        return len;
+    }
+    int bs(vector<int>& end,int num,int len){
+        int l = 0, r = len - 1, m;
+        int ans = -1;
+        
+        while (l <= r)
+        {
+            m = l + (r - l) / 2;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+            if(end[m]>=num){
+                ans = m;
+                r = m - 1;
+            }
+            else{
+                l = m + 1;
+            }
+        }
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
-
-    return 0;
-}
+        return ans;
+    }
+};
