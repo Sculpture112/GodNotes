c12992b8893ce830645bd1c293b6a54f35efcc1f
xxx <1748701882@qq.com>
2026-07-25 14:03:58 +0800

    vault backup: 2026-07-25 14:03:58

--- God/糖果共享.cpp
+++ God/糖果共享.cpp
@@ -8,20 +8,22 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
-
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     cin >> n;
-    vector<pair<int,int>> time(n);
+    vector<pair<int, int>> time(n);
     vector<int> delay(n);
     int mx = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> time[i].first;
         mx = max(time[i].first, mx);
         time[i].second = i;
@@ -35,19 +37,26 @@ int main() {
     int q;
     cin >> q;
     vector<int> quiery(q);
-    for (int i = 0; i < q; i++) {
-        cin>>quiery[i];
+    for (int i = 0; i < q; i++)
+    {
+        cin >> quiery[i];
     }
-    vector<int> ans(n);
-    priority_queue<vector<pair<int,int>>,vector<pair<int,int>>,greater<vector<pair<int,int>>>> pq;
-    for (int i = 1,j=0; i <= mx; i++) {
-        while(time[j].first<=i){
+    vector<int> ans(n + 1, -1);
+    priority_queue<vector<pair<int, int>>, vector<pair<int, int>>, greater<vector<pair<int, int>>>> pq;
+    for (int i = 1, j = 0; i <= mx; i++)
+    {
+        while (j < n && time[j].first <= i)
+        {
             pq.push({delay[time[j].second], time[j].second});
             j++;
         }
-        while(pq.top().first == i)   
+        while (pq.top().first == i)
         {
-            
+            if (ans[pq.top().second + 1] == -1)
+            {
+                pq.push({delay[pq.top().second + 1], pq.top().second + 1});
+                ans[pq.top().second + 1] = i;
+            }
             pq.top();
         }
     }
