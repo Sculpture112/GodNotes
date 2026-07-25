6f0216adf71020695e0cfaf190724aedccab3615
xxx <1748701882@qq.com>
2026-07-25 10:53:20 +0800

    vault backup: 2026-07-25 10:53:19

--- God/母题/087 Code03 两个排列的最长公共子序列长度.cpp
+++ God/母题/087 Code03 两个排列的最长公共子序列长度.cpp
@@ -7,18 +7,55 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
+const int MAXN = 1e5 + 5;
+int a[MAXN],b[MAXN],tail[MAXN];
+int where[MAXN];
 
-void solve() {
-
+int bs(int len,int num){
+    int l = 0, r = len - 1, m, ans = -1;
+    while (l<=r)
+    {
+        m = l + ((r - l) >> 1);
+        if(tail[m]>=num){
+            ans = m;
+            r = m - 1;
+        }
+        else{
+            l = m + 1;
+        }
+    }
+    return ans;
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    for (int i = 0; i < n; i++) {
+        where[a[i]] = i;
+    }
+    for (int i = 0; i < n; i++) {
+        b[i] = where[b[i]];
+    }
 
+    int len = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int find = bs(len, b[i]);
+        if(find ==-1){
+            tail[len++] = b[i];
+        }
+        else{
+            tail[find] = b[i];
+        }
+    }
+    cout << len;
     return 0;
 }
