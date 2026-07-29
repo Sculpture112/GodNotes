61e54e09d0d26f98a1b7fd20ea95ed1ad448020a
xxx <1748701882@qq.com>
2026-07-29 17:39:22 +0800

    vault backup: 2026-07-29 17:39:22

--- God/写题/牛客/2026牛客暑期多校训练营1/e.cpp
+++ God/写题/牛客/2026牛客暑期多校训练营1/e.cpp
@@ -8,27 +8,31 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
-
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
+    vector<ll> arr(n);
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
     ll sum = 0;
-    for (int i = 0,j = n-1; i < n,j>0; i++,j--) {
-        sum -= j * arr[i];
+    for (int i = 0, j = n - 1; i < n && j > 0; i++, j--)
+    {
+        sum -= 1LL * j * arr[i];
     }
-    for (int i = n - 1,j = n-1; i >= 0,j>0;i--,j--){
-        sum += j * arr[i];
+    for (int i = n - 1, j = n - 1; i >= 0 && j > 0; i--, j--)
+    {
+        sum += 1LL * j * arr[i];
     }
     cout << sum;
 
