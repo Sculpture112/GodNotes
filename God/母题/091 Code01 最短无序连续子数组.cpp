22b7658cbc278bd2b04b0c132574daf581cb7397
xxx <1748701882@qq.com>
2026-07-29 15:13:11 +0800

    vault backup: 2026-07-29 15:13:11

--- God/母题/091 Code01 最短无序连续子数组.cpp
+++ God/母题/091 Code01 最短无序连续子数组.cpp
@@ -13,5 +13,30 @@ class Solution
 public:
     int findUnsortedSubarray(vector<int> &nums)
     {
+        int n = nums.size();
+        int right = -1;
+        int maxvalue = INT_MIN;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] < maxvalue)
+            {
+                right = i;
+            }
+            maxvalue = max(maxvalue, nums[i]);
+        }
+
+        int left = n;
+        int minvalue = INT_MAX;
+        for (int i = n - 1; i >= 0; i--)
+        {
+
+            if (nums[i] > minvalue)
+            {
+                left = i;
+            }
+            minvalue = min(minvalue, nums[i]);
+        }
+        return max(0, right - left + 1);
     }
 };
