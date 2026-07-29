797d7a054dc7ee581d1cccd06cf40693503d8f92
xxx <1748701882@qq.com>
2026-07-29 10:23:20 +0800

    vault backup: 2026-07-29 10:23:20

--- God/母题/090 Code04 会议只占一天的最大会议数量.cpp
+++ God/母题/090 Code04 会议只占一天的最大会议数量.cpp
@@ -13,5 +13,32 @@ class Solution
 public:
     int maxEvents(vector<vector<int>> &events)
     {
+        int n = events.size();
+        sort(events.begin(), events.end(), [](const auto &a, const auto &b)
+             { return a[0] < b[0]; });
+
+        int minday = events[0][0];
+        int maxday = events[0][1];
+        for (int i = 1; i < n; i++) {
+            maxday = max(maxday, events[i][1]);
+        }
+
+        priority_queue<int, vector<int>, greater<int>> heap;
+
+        int ans = 0;
+        for (int day = minday, i = 0; day <= maxday; day++)
+        {
+            while (i < n&&events[i][0]==day){
+                heap.push(events[i++][1]);
+            }
+            while(!heap.empty() && heap.top()<day){
+                heap.pop();
+            }
+            if(!heap.empty()){
+                heap.pop();
+                ans++;
+            }
+        }
+        return ans;
     }
 };
