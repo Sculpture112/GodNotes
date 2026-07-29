69d7f84ec57641cae66492f7afcb59d6fa588dd7
xxx <1748701882@qq.com>
2026-07-29 11:02:53 +0800

    vault backup: 2026-07-29 11:02:53

--- God/母题/090 Code05 IPO.cpp
+++ God/母题/090 Code05 IPO.cpp
@@ -11,7 +11,28 @@ const ll LINF = 4e18;
 class Solution
 {
 public:
-    int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
+    int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &cost)
     {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap1;
+        priority_queue<int> heap2;
+        int n = profits.size();
+        for (int i = 0; i < n; i++) {
+            heap1.push({cost[i], profits[i]});
+        }
+
+        while (k--)
+        {
+            while(!heap1.empty()&&  heap1.top().first <=w){
+                heap2.push(heap1.top().second);
+                heap1.pop();
+            }
+            if(heap2.empty()){
+                break;
+            }
+
+            w += heap2.top();
+            heap2.pop();
+        }
+        return w;
     }
 };
