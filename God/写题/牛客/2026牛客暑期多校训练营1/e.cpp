e490eb590d1221e9e5d84b824c42ef3b568aaf91
xxx <1748701882@qq.com>
2026-07-29 17:32:12 +0800

    vault backup: 2026-07-29 17:32:12

--- God/写题/牛客/2026牛客暑期多校训练营1/e.cpp
+++ God/写题/牛客/2026牛客暑期多校训练营1/e.cpp
@@ -16,9 +16,14 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    
 
     return 0;
 }
