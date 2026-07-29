69c901f29d7aa6d03720e70251da00309a2deb64
xxx <1748701882@qq.com>
2026-07-29 16:03:21 +0800

    vault backup: 2026-07-29 16:03:21

--- God/母题/091 Code02 最小区间.cpp
+++ God/母题/091 Code02 最小区间.cpp
@@ -8,17 +8,40 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+class Solution
+{
+public:
+    struct Node{
+        int v, i, j;
+    };
+    struct Compare{
+        bool operator()(const Node& a,const Node& b)const{
+            return a.v != b.v ? a.v < b.v : a.i < b.i;
+        }
+    };
 
-}
+    vector<int> smallestRange(vector<vector<int>> &nums)
+    {
+        int k = nums.size(),width =INT_MAX,right = 0,left =0;
+        set<Node, Compare> set;
+        for (int i = 0; i < k;i++)
+            set.insert({nums[i][0], i, 0});
+        
+        while(set.size() == k){
+            Node maxNode = *set.rbegin();
+            Node minNode = *set.begin();
+            set.erase(set.begin());
+            if(maxNode.v - minNode.v<width){
+                width = maxNode.v - minNode.v;
+                left = minNode.v;
+                right = maxNode.v;
+            }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
-
-    return 0;
-}
+            if(minNode.j + 1<nums[minNode.i].size())
+            {
+                set.insert({nums[minNode.i][minNode.j + 1], minNode.i, minNode.j + 1});
+            }
+        }
+        return {left, right};
+    }
+};
