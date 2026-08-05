df10d3dfba13eafd607d7407f30392d5b0d04b5c
xxx <1748701882@qq.com>
2026-08-05 17:16:00 +0800

    vault backup: 2026-08-05 17:16:00

--- God/母题/092 Code04 知识竞赛.cpp
+++ God/母题/092 Code04 知识竞赛.cpp
@@ -7,18 +7,31 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
+const int MAXN = 200001;
+struct Employee
+{
+    int a;
+    int b;
+};
 
-void solve() {
-
+Employee nums[MAXN];
+void solve()
+{
+    sort(nums.begin(), nums.end(), (const Employee &a, const Employee &b) { return abs(a.a - a.b) < abs(b.a - b.b); });
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i].a >> nums[i].b;
+    }
+    solve;
 
     return 0;
 }
