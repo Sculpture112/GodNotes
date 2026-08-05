6fd469521da21be248f8302437d1285cef7a5a89
xxx <1748701882@qq.com>
2026-08-05 19:28:07 +0800

    vault backup: 2026-08-05 19:28:07

--- God/母题/093 Code02 灌溉花园的最少水龙头数目.cpp
+++ God/母题/093 Code02 灌溉花园的最少水龙头数目.cpp
@@ -13,6 +13,33 @@ class Solution
 public:
     int minTaps(int n, vector<int> &ranges)
     {
-        
+        vector<int> right(n + 1, 0);
+        for (int i = 0, start; i <= n; i++)
+        {
+            start = max(0, i - ranges[i]);
+            right[start] = max(right[start], i + ranges[i]);
+        }
+
+        int ans = 0;
+        int cur = 0;
+        int nxt = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            nxt = max(nxt, right[i]);
+            if (cur == i)
+            {
+                if (nxt > i)
+                {
+                    ans++;
+                    cur = nxt;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+        return ans;
     }
 };
