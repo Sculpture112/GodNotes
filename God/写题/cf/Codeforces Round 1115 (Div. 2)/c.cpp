8f15fe5786173bb03417e92d00ca7c7d01600548
xxx <1748701882@qq.com>
2026-08-07 00:09:09 +0800

    vault backup: 2026-08-07 00:09:09

--- God/写题/cf/Codeforces Round 1115 (Div. 2)/c.cpp
+++ God/写题/cf/Codeforces Round 1115 (Div. 2)/c.cpp
@@ -32,13 +32,13 @@ void solve()
     vector<int> count(n + 1, -1);
     int ans = m;
     priority_queue<int> heap,heap2;
-    for (int i = n; i >= 1; i--)
+    for (int i = n,sum =0; i >= 1; i--)
     {
         for (int j = 1; j <= m;j++){
             heap.push(graph[i][j]);
         }
         heap2 = heap;
-        
+        while(sum)
         heap = heap2;
     }
     cout << ans << "\n";
