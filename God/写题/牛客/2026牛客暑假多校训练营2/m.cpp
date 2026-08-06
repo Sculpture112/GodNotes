68505515ce26bb6fd012c19755c794ecd1a93a83
xxx <1748701882@qq.com>
2026-08-06 16:32:50 +0800

    vault backup: 2026-08-06 16:32:50

---
+++ God/写题/牛客/2026牛客暑假多校训练营2/m.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
