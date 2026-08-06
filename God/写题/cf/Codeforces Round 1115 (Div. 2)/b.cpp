56f6412df1f5efecdee477cb5455ca019a9396e5
xxx <1748701882@qq.com>
2026-08-06 23:29:17 +0800

    vault backup: 2026-08-06 23:29:17

---
+++ God/写题/cf/Codeforces Round 1115 (Div. 2)/b.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int c0 = 0, c1 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '0')
+            c0++;
+        else
+        {
+            c1++;
+        }
+    }
+
+    if (abs(c1 - c0) > 2)
+    {
+        cout << -1 << "\n";
+        return;
+    }
+
+    c1 = 0, c0 = 0;
+    ll ans = 0;
+
+    for (int i = 0, j; i < n; j++)
+    {
+        j = i + 1;
+
+        while (j < n && s[j] == s[i])
+        {
+            j++;
+        }
+
+        if (j - i - 1 > 0 && s[i] == '0')
+        {
+            c0 += j - i - 1;
+        }
+        else if (j - i - 1 > 0 && s[i] == '1')
+        {
+            c1 += j - i - 1;
+        }
+        ans += j - i - 1;
+        i = j;
+    }
+    if (abs(c1 - c0) >= 2)
+    {
+        ans += (max(c1, c0) - 1);
+    }
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
