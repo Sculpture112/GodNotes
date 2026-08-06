d0d5dee7e33bac0d52bb465c0b354f9b59b10185
xxx <1748701882@qq.com>
2026-08-06 23:57:03 +0800

    vault backup: 2026-08-06 23:57:03

--- God/写题/cf/Codeforces Round 1115 (Div. 2)/c.cpp
+++ God/写题/cf/Codeforces Round 1115 (Div. 2)/c.cpp
@@ -8,34 +8,42 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n, m;
     cin >> n >> m;
-    vector<int> weight(n+1);
-    for (int i = 0; i < n; i++) {
+    vector<int> weight(n);
+    for (int i = 0; i < n; i++)
+    {
         cin >> weight[i];
     }
 
     vector<vector<int>> graph(n + 1, vector<int>(m + 1));
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m;j++){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
             cin >> graph[i][j];
         }
     }
     vector<int> count(n + 1, -1);
     vector<int> srt;
     int ans = m;
-    for (int i = n; i >= 1;i--){
-        for (int j = 1; j <= m;j++){
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= m; j++)
+        {
             srt.push_back(graph[i][j]);
         }
         int sum = 0;
         int cnt = 0;
         sort(srt.rbegin(), srt.rend());
-        for(int num:srt){
+        for (int num : srt)
+        {
             sum += num;
             cnt++;
-            if(sum>=weight[i]){
+            if (sum >= weight[i - 1])
+            {
                 count[i] = cnt;
                 // cout << cnt;
                 ans = min(ans, count[i]);
@@ -46,13 +54,15 @@ void solve() {
     cout << ans << "\n";
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
