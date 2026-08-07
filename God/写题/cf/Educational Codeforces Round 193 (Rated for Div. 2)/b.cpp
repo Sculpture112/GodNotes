6164869f5e233f22eb2ce00f1dc64185dc48c7b8
xxx <1748701882@qq.com>
2026-08-07 23:10:01 +0800

    vault backup: 2026-08-07 23:10:01

--- God/写题/cf/Educational Codeforces Round 193 (Rated for Div. 2)/b.cpp
+++ God/写题/cf/Educational Codeforces Round 193 (Rated for Div. 2)/b.cpp
@@ -8,7 +8,8 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
     vector<int> a(n);
@@ -16,40 +17,53 @@ void solve() {
     int kinds = 0;
     int sim = 0;
     int pre = -1;
-    for(int &num:a)
+    for (int &num : a)
     {
         cin >> num;
-        if(pre = num)
+        if (pre = num)
             sim++;
-        if(cnt[num]++ == 0){
+        if (cnt[num]++ == 0)
+        {
             kinds++;
         }
         pre = num;
     }
 
-    if(kinds == 1){
+    if (kinds == 1)
+    {
         cout << 1 << "\n";
         return;
     }
 
     pre = a[0];
     int sta = -1;
-    for (int i = 1; i < n; i++) {
-        if(a[i] == pre && a[i+1] != a[i]){
-            if(a[i+2] != a[i]){
-                sta = 2;
+    for (int i = 1; i < n; i++)
+    {
+        if(sta == 2)
+            break;
+        if (a[i] == pre && a[i + 1] != a[i])
+        {
+            if (a[i + 2] != a[i])
+            {
+                if (a[i + 1] == a[i + 2])
+                    sta == 2;
+
+                else
+                    sta = 1;
             }
         }
     }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
