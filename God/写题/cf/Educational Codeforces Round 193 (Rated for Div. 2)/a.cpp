544b6e293ba0573fdf85161d6214519875ad1a02
xxx <1748701882@qq.com>
2026-08-07 22:48:13 +0800

    vault backup: 2026-08-07 22:48:13

--- God/写题/cf/Educational Codeforces Round 193 (Rated for Div. 2)/a.cpp
+++ God/写题/cf/Educational Codeforces Round 193 (Rated for Div. 2)/a.cpp
@@ -5,20 +5,37 @@ using ll = long long;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
-
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
-
+bool isPrime(int x)
+{
+    if (x % 2 == 0)
+        return false;
+    for (int i = 3; i * i <= x; i += 2)
+    {
+        if (x % i == 0)
+            return false;
+    }
+    return true;
+}
+bool solve()
+{
+    int n;
+    cin >> n;
+    if (n == 2)
+        return true;
+    return isPrime(n + 1);
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    cin >> T;
+    while (T--)
+        cout << (solve() ? "YES" : "NO") << "\n";
 
     return 0;
 }
