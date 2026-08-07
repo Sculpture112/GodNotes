d6b42741485d4bcd0de5d73641c6111c213c9c82
xxx <1748701882@qq.com>
2026-08-07 23:04:14 +0800

    vault backup: 2026-08-07 23:04:14

--- God/写题/cf/Educational Codeforces Round 193 (Rated for Div. 2)/b.cpp
+++ God/写题/cf/Educational Codeforces Round 193 (Rated for Div. 2)/b.cpp
@@ -9,7 +9,38 @@ const ll LINF = 4e18;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> cnt(n);
+    int kinds = 0;
+    int sim = 0;
+    int pre = -1;
+    for(int &num:a)
+    {
+        cin >> num;
+        if(pre = num)
+            sim++;
+        if(cnt[num]++ == 0){
+            kinds++;
+        }
+        pre = num;
+    }
 
+    if(kinds == 1){
+        cout << 1 << "\n";
+        return;
+    }
+
+    pre = a[0];
+    int sta = -1;
+    for (int i = 1; i < n; i++) {
+        if(a[i] == pre && a[i+1] != a[i]){
+            if(a[i+2] != a[i]){
+                sta = 2;
+            }
+        }
+    }
 }
 
 int main() {
