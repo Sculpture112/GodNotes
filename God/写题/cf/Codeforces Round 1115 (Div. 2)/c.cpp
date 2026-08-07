2b8c9edcbfc2da00f1e0cbb535b4ce588106b18b
xxx <1748701882@qq.com>
2026-08-07 15:06:38 +0800

    vault backup: 2026-08-07 15:06:38

--- God/写题/cf/Codeforces Round 1115 (Div. 2)/c.cpp
+++ God/写题/cf/Codeforces Round 1115 (Div. 2)/c.cpp
@@ -8,46 +8,51 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-int solve() {
+int solve()
+{
     int n, m;
     cin >> n >> m;
     vector<ll> v(n);
-    for (ll &x: v)
+    for (ll &x : v)
         cin >> x;
 
     vector<ll> arr(1LL * n * m);
     priority_queue<ll, vector<ll>, greater<ll>> heap;
-    for(ll &x :arr)
+    for (ll &x : arr)
         cin >> x;
     ll sum = 0;
     ll ans = m;
-    for (int i = n-1; i >=0 ;i--){
-        for (int j = 0; j < m;j++){
-            heap.push(arr[1LL * n * m]);
-            sum += arr[1LL * n * m];
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            heap.push(arr[1LL * i * m +j]);
+            sum += arr[1LL * i * m +j];
+            if (heap.size() == ans)
+            {
+                sum -= heap.top();
+                heap.pop();
+            }
         }
-        if(heap.size() == ans){
-            sum -= heap.top();
-            heap.pop();
-
-        }
-        while(!heap.empty()&&sum>=v[i]){
+        while (!heap.empty() && sum >= v[i])
+        {
             ans = heap.size();
             sum -= heap.top();
             heap.pop();
-
         }
     }
     return ans;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     cin >> T;
-    while (T--) cout<<solve()<<"\n";
+    while (T--)
+        cout << solve() << "\n";
 
     return 0;
 }
