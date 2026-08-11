6579215b0f46d7bd85e8d92a4e8079b846dfd0ef
xxx <1748701882@qq.com>
2026-08-11 19:59:29 +0800

    vault backup: 2026-08-11 19:59:28

--- God/P_4447_AHOI_2018_初中组_分组.cpp
+++ God/P_4447_AHOI_2018_初中组_分组.cpp
@@ -11,7 +11,32 @@ const ll LINF = 4e18;
 void solve() {
     int n;
     cin >> n;
+    vector<ll> a(n);
+    for(ll &x:a)
+        cin >> x;
+    sort(a.begin(), a.end());
+
+    using minHeap = priority_queue<int, vector<int>, greater<int>>;
+    unordered_map<ll, minHeap> group;
     
+    for(ll &x :a){
+        int len = 1;
+
+        auto it = group.find(x - 1);
+        if(it!=group.end() && !it->second.empty()){
+            len = it->second.top() + 1;
+            it->second.pop();
+        }
+        group[x].push(len);
+    }
+
+    ll ans = n;
+    for(auto &[_,heap]:group){
+        if(!heap.empty()){
+            ans = min(ans, heap.top());
+        }
+    }
+    cout << ans;
 }
 
 int main() {
