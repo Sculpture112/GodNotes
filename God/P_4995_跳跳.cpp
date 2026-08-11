1950369779444e2ff1e7fb4de8289f481a6b2e6d
xxx <1748701882@qq.com>
2026-08-11 17:03:43 +0800

    vault backup: 2026-08-11 17:03:43

--- God/P_4995_跳跳.cpp
+++ God/P_4995_跳跳.cpp
@@ -8,17 +8,38 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &x : a)
+        cin >> x;
+    sort(a.rbegin(), a.rend());
+    ll ans = a[0] * a[0];
 
+    for (int k = 1,i=0,j=n-1; k <= n,i<=j; k++) {
+        if(k&1 != 0){
+            ans += (a[i] - a[j]) * (a[i] - a[j]);
+            i++;
+        }
+        else{
+            ans += (a[i] - a[j]) * (a[i] - a[j]);
+            j--;
+        }
+    }
+    cout << ans;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
