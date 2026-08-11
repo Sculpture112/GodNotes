c4e41e5b24c02b26668629a675261ab370cfe7f3
xxx <1748701882@qq.com>
2026-08-11 16:20:39 +0800

    vault backup: 2026-08-11 16:20:39

--- God/P_1106_删数问题.cpp
+++ God/P_1106_删数问题.cpp
@@ -8,29 +8,48 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
-    string str;
-    cin >> str;
+void solve()
+{
+    string n;
     int k;
-    cin >> k;
-    vector<char> s;
-    int n = str.size();
-    for (int i = 0; i < str.length(); i++) {
-        s.push_back(str[i]);
+
+    cin >> n >> k;
+    string ans;
+    for (int i = 0; i < n.size(); i++)
+    {
+        while (!ans.empty() && ans.back() > n[i])
+        {
+            ans.pop_back();
+            k--;
+        }
+        ans.push_back(n[i]);
     }
-    sort(s.begin(), s.end());
-    for (int i = 0; i < n - k;i++){
-        cout << s[i];
+
+    while (k > 0)
+    {
+        ans.pop_back();
+        k--;
+    }
+    size_t pos = ans.find_first_not_of('0');
+    if (pos == string::npos)
+    {
+        cout << 0 << "\n";
+    }
+    else
+    {
+        cout << ans.substr(pos) << "\n";
     }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
