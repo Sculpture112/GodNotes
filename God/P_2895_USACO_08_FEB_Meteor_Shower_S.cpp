30ca9302a1184206de356424ec18c1fa9496684f
xxx <1748701882@qq.com>
2026-08-13 18:58:38 +0800

    vault backup: 2026-08-13 18:58:37

--- God/P_2895_USACO_08_FEB_Meteor_Shower_S.cpp
+++ God/P_2895_USACO_08_FEB_Meteor_Shower_S.cpp
@@ -3,24 +3,57 @@ using namespace std;
 
 using ll = long long;
 
+const int N = 305;
 const int INF = 0x3f3f3f3f;
+
 const ll LINF = 4e18;
+int dangertime[N][N];
+int dist[N][N];
 
 #define all(x) (x).begin(), (x).end()
 int graph[301][301];
-void solve() {
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+void solve()
+{
     int n;
     cin >> n;
+    memset(dangertime, 0x3f, sizeof dangertime);
+    memset(dist, -1, sizeof dist);
 
+    for (int i = 0; i < n; i++)
+    {
+        int x, y, t;
+        cin >> x >> y >> t;
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+
+            if (nx >= 0 && nx <= n && ny >= 0 && ny <= n)
+            {
+                dangertime[nx][ny] = min(dangertime[nx][ny], t);
+            }
+        }
+    }
+
+    if(dangertime[0][0] == 0){
+        cout << -1 << "\n";
+        return;
+    }
+
+    queue<
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
