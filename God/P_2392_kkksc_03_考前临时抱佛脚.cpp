13f3dd4d91ff1a1418f52090877a2606d2774000
xxx <1748701882@qq.com>
2026-08-13 15:00:34 +0800

    vault backup: 2026-08-13 15:00:34

---
+++ God/P_2392_kkksc_03_考前临时抱佛脚.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int s1, s2, s3, s4;
+    cin >> s1 >> s2 >> s3 >> s4;
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
