eb3172fe55b1bb8cab1572659ac35963b8caf055
xxx <1748701882@qq.com>
2026-08-13 19:39:48 +0800

    vault backup: 2026-08-13 19:39:48

--- God/P_1036_NOIP_2002_普及组_选数.cpp
+++ God/P_1036_NOIP_2002_普及组_选数.cpp
@@ -8,27 +8,58 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 const int N = 24;
-vector<bool> IsPrime(N,true);
-void init(){
+vector<bool> IsPrime(N, true);
+
+int n, k;
+int ans = 0;
+vector<int> arr(N);
+void init()
+{
     IsPrime[0] = false, IsPrime[1] = false;
-    for(int i = )
+    for (int i = 2; 1ll * i * i <= N; i++)
+    {
+        if (!IsPrime[i])
+            continue;
+        for (int j = i * i; j <= N; j += i)
+        {
+            IsPrime[j] = false;
+        }
+    }
 }
-void solve() {
-    int n, k;
+void dfs(int idx, int select, int sum)
+{
+    if (select == k && IsPrime[sum])
+    {
+        ans++;
+        return;
+    }
+
+    if (idx == n)
+        return;
+
+    dfs(idx + 1, select + 1, sum += arr[idx]);
+    dfs(idx + 1, select, sum);
+}
+void solve()
+{
     cin >> n >> k;
-    vector<int> arr(n);
 
-    for(int &x:arr)
+    for (int &x : arr)
         cin >> x;
+
+    init();
+    cout << ans;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
