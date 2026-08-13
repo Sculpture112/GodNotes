46d036c263b7a11e6d3c675d12e3d20a431954b2
xxx <1748701882@qq.com>
2026-08-13 15:41:32 +0800

    vault backup: 2026-08-13 15:41:32

--- God/P_1443_马的遍历.cpp
+++ God/P_1443_马的遍历.cpp
@@ -8,19 +8,45 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
-    int n, m, x, y;
-    cin >> n >> m >> x >> y;
-    
+int n, m;
+int arr[401][401];
+int step = 0;
+int dx[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+int dy[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+void dfs(int x, int y)
+{
+    if (x > n || y > m || x < 0 || y < 0)
+        return;
+    arr[x][y] = step;
+    for (int i = 0; i < 8; i++) {
+        dfs(dx[i] + x)(dy[i] + y);
+    }
 }
 
-int main() {
+void solve()
+{
+    int x, y;
+    cin >> n >> m >> x >> y;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            arr[i][j] = -1;
+        }
+    }
+    x--, y--;
+    dfs(x, y);
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
