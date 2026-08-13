cf6ab4c7e48b2fc2c904fd184a6e91e64e4a52fb
xxx <1748701882@qq.com>
2026-08-13 19:32:57 +0800

    vault backup: 2026-08-13 19:32:57

---
+++ God/P_1036_NOIP_2002_普及组_选数.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+const int N = 24;
+vector<bool> IsPrime(N,true);
+void init(){
+    IsPrime[0] = false, IsPrime[1] = false;
+    for(int i = )
+}
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+
+    for(int &x:arr)
+        cin >> x;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
