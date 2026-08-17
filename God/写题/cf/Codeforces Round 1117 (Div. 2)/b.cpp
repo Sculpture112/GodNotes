4f856d3f2144a46e30ab940a980ed4d9f1f3a952
xxx <1748701882@qq.com>
2026-08-17 23:12:07 +0800

    vault backup: 2026-08-17 23:12:07

--- God/写题/cf/Codeforces Round 1117 (Div. 2)/b.cpp
+++ God/写题/cf/Codeforces Round 1117 (Div. 2)/b.cpp
@@ -8,26 +8,50 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n, m;
     cin >> n >> m;
     vector<int> a(n);
-    for(int &x:a)
-        cin >> x;
-    vector<int> b(m);
-    for(int&x:b)
-        cin >> x;
-    
+    int sum = 0;
 
+    for (int &x : a)
+    {
+        cin >> x;
+    }
+    for (int i = 1; i < n; i++)
+    {
+        sum += a[i - 1] - a[i] + 1;
+    }
+    sum += a[n - 1];
+    vector<int> b(m);
+    for (int &x : b)
+        cin >> x;
+    int sun = 0;
+    for (int i = 1; i < m; i++)
+    {
+        sun += abs(b[i] - b[i - 1]) + 1;
+    }
+    sun += b[m - 1];
+    if (sum < sun)
+    {
+        cout << 2 << "\n";
+    }
+    else
+    {
+        cout << 1 << "\n";
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
