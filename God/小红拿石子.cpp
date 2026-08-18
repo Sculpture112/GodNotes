fc9bcf3c37d9ac71cc01294a140162b445de8c23
xxx <1748701882@qq.com>
2026-08-18 15:15:38 +0800

    vault backup: 2026-08-18 15:15:38

---
+++ God/小红拿石子.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int&x:a)
+        cin >> x;
+
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
