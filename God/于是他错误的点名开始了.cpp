48fccb96489c9fb5a2227d7e3761f7396d2feab6
xxx <1748701882@qq.com>
2026-08-31 14:20:41 +0800

    vault backup: 2026-08-31 14:20:41

--- God/于是他错误的点名开始了.cpp
+++ God/于是他错误的点名开始了.cpp
@@ -8,17 +8,49 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
+    int n;
+    cin >> n;
+    map<string, int> rema;
+    map<string, int> ans;
+    string s;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s;
+        rema[s]++;
+    }
+    int m;
+    cin >> m;
 
+    for (int i = 0; i < m; i++)
+    {
+        cin >> s;
+        if (rema.count(s) && (ans[s]++ == 0))
+        {
+            cout << "OK" << "\n";
+        }
+
+        else if (rema.count(s) && (ans[s]++ != 0))
+        {
+            cout << "REPEAT" << "\n";
+        }
+        else
+        {
+            cout << "WRONG" << "\n";
+        }
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
