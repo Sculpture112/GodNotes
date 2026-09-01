7ffdd47872e610e0a48f4349930915a51649866b
xxx <1748701882@qq.com>
2026-09-01 12:07:10 +0800

    vault backup: 2026-09-01 12:07:10

---
+++ God/P_1481_魔族密码.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<string> a(n);
+    for(string& s:a){
+        cin >> s;
+    }
+
+    vector<int> dp(n + 1);
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
