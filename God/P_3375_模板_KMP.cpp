d79b714da53af5c947c2ed9570546af223c0eb19
xxx <1748701882@qq.com>
2026-09-01 09:34:23 +0800

    vault backup: 2026-09-01 09:34:23

--- God/P_3375_模板_KMP.cpp
+++ God/P_3375_模板_KMP.cpp
@@ -8,17 +8,59 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
+    string text, pattern;
+    cin >> text >> pattern;
 
+    text = " " + text;
+    pattern = " " + pattern;
+    int n = text.size() - 1;
+    int m = pattern.size() - 1;
+
+    vector<int> nxt(m + 1);
+    for (int i = 2, j = 0; i <= m; i++)
+    {
+        while (j > 0 && pattern[i] != pattern[j + 1])
+        {
+            j = nxt[j];
+        }
+
+        if (pattern[i] == pattern[j + 1])
+        {
+            j++;
+        }
+
+        nxt[i] = j;
+    }
+
+    for (int i = 1, j = 0; i <= n; i++)
+    {
+        while (j > 0 && pattern[j + 1] != text[i])
+        {
+            j = nxt[j];
+        }
+
+        if (pattern[j + 1] == text[i])
+        {
+            j++;
+        }
+
+        if(j ==m){
+            cout<<
+        }
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
