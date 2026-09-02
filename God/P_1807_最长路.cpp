60fd9eb8b5652e4bf3c0fb7ad6dcb26b76251f85
xxx <1748701882@qq.com>
2026-09-02 09:56:09 +0800

    vault backup: 2026-09-02 09:56:09

--- God/P_1807_最长路.cpp
+++ God/P_1807_最长路.cpp
@@ -8,17 +8,38 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<int, int>>> graph(n + 1);
+    for (int i = 0, u, v, w; i < m; i++)
+    {
+        cin >> u >> v >> w;
+        graph[u].push_back({v, w});
+    }
+    vector<int> dp(n + 1, 0);
 
+    for (int u = 1; u <= n; u++)
+    {
+        if(dp[u] == 0)continue;
+        for(auto[v,w]:graph[u]){
+            dp[v] = max(dp[v], dp[u] + 1);
+        }
+    }
+
+    
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
