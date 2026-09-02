f8efe03b140a11112b432c63bb2714ababa38d65
xxx <1748701882@qq.com>
2026-09-02 10:18:13 +0800

    vault backup: 2026-09-02 10:18:12

--- God/P_2853_USACO_06_DEC_Cow_Picnic_S.cpp
+++ God/P_2853_USACO_06_DEC_Cow_Picnic_S.cpp
@@ -9,7 +9,43 @@ const ll LINF = 4e18;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
+    int k, n, m;
+    cin >> k >> n >> m;
+    vector<int> cows(k);
+    for(int& num:cows)
+        cin >> num;
 
+    vector<vector<int>> graph(n+1);
+    for (int i = 0,u,v; i < m; i++) {
+        cin >> u >> v;
+        graph[u].push_back(v);
+    }
+
+    vector<int> reached(n + 1);
+    for(int num:cows){
+        vector<bool> isivisted(n + 1, false);
+        queue<int> q;
+        q.push(num);
+        isivisted[num] = true;
+        while(!q.empty()){
+            int num = q.front();
+            q.pop();
+            reached[num]++;
+            for(int v : graph[num]){
+                if(!isivisted[v]){
+                    q.push(v);
+                    isivisted[v] = true;
+                }
+            }
+        }
+    }
+    int ans = 0;
+    for(int num :reached){
+        if(num == k){
+            ans++;
+        }
+    }
+    cout << ans;
 }
 
 int main() {
