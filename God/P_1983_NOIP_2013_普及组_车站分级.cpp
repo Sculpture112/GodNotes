c10765adbf0043cde83e26cd8945dfe717fc4264
xxx <1748701882@qq.com>
2026-09-03 20:37:00 +0800

    vault backup: 2026-09-03 20:37:00

--- God/P_1983_NOIP_2013_普及组_车站分级.cpp
+++ God/P_1983_NOIP_2013_普及组_车站分级.cpp
@@ -7,18 +7,17 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
-
-void solve() {
-
-}
+const int MAXN = 1005;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    int n, m;
+    cin >> n >> m;
 
+    vector<bitset<MAXN>> adj(n + 1);
+    
+    
     return 0;
 }
