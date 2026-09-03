d05e124b4ff3ca0e753c20b90ef7d7d9a5de7bdc
xxx <1748701882@qq.com>
2026-09-03 19:35:20 +0800

    vault backup: 2026-09-03 19:35:20

--- God/P_1347_ECNA_2001_排序.cpp
+++ God/P_1347_ECNA_2001_排序.cpp
@@ -8,17 +8,44 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+int topo(int n,bool graph[26][26],string& word){
+    int indegree[26]={};
 
+    for (int i = 0; i < n;i++){
+        for (int j = 0; j < n;j++){
+            if(graph[i][j])
+                indegree[j]++;
+        }
+    }
+
+    
 }
-
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    int n, m;
+    cin >> n >> m;
+
+    bool graph[26][26];
+
+    for (int relation = 1; relation <= m; relation++)
+    {
+        string s;
+        cin >> s;
+        int u = s[0] - 'A';
+        int v = s[2] - 'A';
+
+        if(!graph[u][v])
+        {
+            graph[u][v] = true;
+            continue;
+        }
+
+        string order;
+        int result = 
+    }
 
     return 0;
 }
