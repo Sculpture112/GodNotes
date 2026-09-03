63a82ba5ac6d155791de734a2ef564e8a17f8a9f
xxx <1748701882@qq.com>
2026-09-03 15:22:50 +0800

    vault backup: 2026-09-03 15:22:50

--- God/P_1347_ECNA_2001_排序.cpp
+++ God/P_1347_ECNA_2001_排序.cpp
@@ -8,11 +8,13 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 bool reach[26][26];
-int solve() {
+int solve()
+{
     int n, m;
     cin >> n >> m;
 
-    for (int step = 1; step <= m;step++){
+    for (int step = 1; step <= m; step++)
+    {
         char a, op, b;
         cin >> a >> op >> b;
 
@@ -21,14 +23,16 @@ int solve() {
 
         reach[u][v] = true;
 
-        for (int k = 0; k < 26;k++)
-            for (int i = 0; i < 26;i++)
-                for (int j = 0; j < 26;j++)
+        for (int k = 0; k < 26; k++)
+            for (int i = 0; i < 26; i++)
+                for (int j = 0; j < 26; j++)
                     reach[i][j] = reach[i][j] || (reach[i][k] && reach[k][j]);
 
         bool huan = false;
-        for (int i = 0; i < n;i++){
-            if(reach[i][i]){
+        for (int i = 0; i < n; i++)
+        {
+            if (reach[i][i])
+            {
                 huan = true;
                 break;
             }
@@ -42,31 +46,54 @@ int solve() {
         }
 
         bool determine = true;
-        for (int i = 0; i < n&&determine;i++){
+        for (int i = 0; i < n && determine; i++)
+        {
 
-            for (int j = i+1; j < n;j++){
-                if(!reach[i][j] && !reach[j][i]){
+            for (int j = i + 1; j < n; j++)
+            {
+                if (!reach[i][j] && !reach[j][i])
+                {
                     determine = false;
                     break;
                 }
             }
         }
 
-        if(determine){
-            string ans(n,' ');
+        if (determine)
+        {
+            string ans(n, ' ');
 
-            for (int i = 0;i<)
+            for (int i = 0; i < n; i++)
+            {
+                int pres = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (reach[j][i])
+                    {
+                        pres++;
+                    }
+                }
+                ans[pres] = i + 'A';
+            }
+            cout << "Sorted sequence determined after "
+                 << step << " relations: "
+                 << ans << ".\n";
+            return 0;
         }
     }
+    cout << "Sorted sequence cannot be determined.\n";
+    return 0;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
