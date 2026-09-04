566719f73d544e04891b1d765189bc58db76d73d
xxx <1748701882@qq.com>
2026-09-04 17:02:43 +0800

    vault backup: 2026-09-04 17:02:43

--- God/P_1596_USACO_10_OCT_Lake_Counting_S.cpp
+++ God/P_1596_USACO_10_OCT_Lake_Counting_S.cpp
@@ -10,23 +10,28 @@ const ll LINF = 4e18;
 
 int n, m;
 int ans = 0;
-const int dx[8]={0,1,1,1,0,-1,-1,-1};
-const int dy[8]={1,1,0,-1,-1,-1,}
-void dfs(){
-    
+const int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+void dfs(int x,int y)
+{
+    for()
 }
 void solve()
 {
     cin >> n >> m;
     vector<string> graph;
     vector<vector<bool>> used(n, vector<bool>(m));
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> graph[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m;j++){
-            if(graph[i][j] == 'W' && !used[i][j]){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (graph[i][j] == 'W' && !used[i][j])
+            {
                 dfs();
             }
         }
