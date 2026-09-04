0065b71422da324c81581f699dd9f227105a2bc3
xxx <1748701882@qq.com>
2026-09-04 09:51:28 +0800

    vault backup: 2026-09-04 09:51:28

--- God/P_1433_吃奶酪.cpp
+++ God/P_1433_吃奶酪.cpp
@@ -5,20 +5,61 @@ using ll = long long;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
-
+const double DINF = 1e100;
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
-
-}
-
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    int n;
+    cin >> n;
+    vector<int> x(n), y(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x[i] >> y[i];
+    }
+    vector<double> diststart(n);
 
-    return 0;
+    for (int i = 0; i < n; i++)
+    {
+        diststart[i] = (x[i] * x[i] + y[i] * y[i]);
+    }
+
+    auto dist = [&](int a, int b)
+    {
+        double dx = (x[a] - x[b]);
+        double dy = (y[a] - y[b]);
+        return (dx * dx + dy * dy);
+    };
+
+    vector<vector<double>> distance(n, vector<double>(n, DINF));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            distance[i][j] = dist(i, j);
+        }
+    }
+
+    vector<vector<double>> dp(n, vector<double>(n, DINF));
+
+    for (int i = 0; i < n;i++){
+        dp[1 << i][i] = diststart[i];
+    }
+
+    int total = 1 << n;
+    for (int mask = 1; mask < total;mask++){
+        for (int last = 0; last < n;last++){
+            if(!(mask & (1<<last))) continue;
+
+            for (int nxt = 0; nxt < n;nxt++){
+                if(mask & (1<<nxt)) continue;
+
+                int newmask = 
+            }
+        }
+    }
+        return 0;
 }
