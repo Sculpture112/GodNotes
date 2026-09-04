abf904fee5c48f5af6493b0d7f8269e7614cdb4a
xxx <1748701882@qq.com>
2026-09-04 16:29:46 +0800

    vault backup: 2026-09-04 16:29:46

--- God/P_2404_自然数的拆分问题.cpp
+++ God/P_2404_自然数的拆分问题.cpp
@@ -8,17 +8,40 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+int n;
 
+
+void dfs(int least, int last)
+{
+    if(least == 0){
+        cout << "\n";
+        return;
+    }
+    for (int i = last; i < least;i++){
+        cout << '+' << i;
+        dfs(least - i, i);
+    }
+}
+void solve()
+{
+    cin >> n;
+
+    for (int i = 1; i < n; i++)
+    {
+        cout << i;
+        dfs(n - i, i);
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
