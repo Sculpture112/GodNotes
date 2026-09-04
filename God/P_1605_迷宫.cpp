d9caa26ec34956ba544fa9d4cd4c8cdcce355acf
xxx <1748701882@qq.com>
2026-09-04 10:21:07 +0800

    vault backup: 2026-09-04 10:21:06

--- God/P_1605_迷宫.cpp
+++ God/P_1605_迷宫.cpp
@@ -7,28 +7,57 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
-
-void solve() {
-    int n, m, t;
+int graph[6][6];
+const int dx[4] = {0, 1, 0, -1};
+const int dy[4] = {1, 0, -1, 0};
+int n, m, t;
+int ans = 0;
+int a, b, c, d;
+int visited[6][6];
+void dfs(int x, int y)
+{
+    if (x < 0 || x >= n || y < 0 || y >= m)
+    {
+        return;
+    }
+    if(x == c && y == d){
+        ans++;
+        return;
+    }
+    for (int i = 0; i < 4;i++){
+        for (int j = 0; j < 4;j++){
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+            visited[nx][ny] = true;
+            dfs(nx, ny);
+            visited[nx][ny] = false;
+        }
+    }
+}
+void solve()
+{
     cin >> n >> m >> t;
-    vector<vector<int>> graph(n, vector<int>(m));
-    int a, b, c, d;
     cin >> a >> b >> c >> d;
-    for (int i = 0; i < t;i++){
+    for (int i = 0; i < t; i++)
+    {
         int x, y;
         cin >> x >> y;
         graph[x][y] = -1;
     }
-    
+    dfs(a, b);
+    visited[a][b] = true;
+    cout << ans;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
