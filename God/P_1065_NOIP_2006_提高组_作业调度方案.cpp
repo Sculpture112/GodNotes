b0966927dc6f605770f9a88050baf3c5bb1a2530
xxx <1748701882@qq.com>
2026-09-05 16:12:11 +0800

    vault backup: 2026-09-05 16:12:11

--- God/P_1065_NOIP_2006_提高组_作业调度方案.cpp
+++ God/P_1065_NOIP_2006_提高组_作业调度方案.cpp
@@ -10,7 +10,57 @@ const double DINF = 1e100;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
-    
+    int m, n;
+    cin >> m >> n;
+
+    int total = m * n;
+
+    // 给定的操作安排顺序
+    vector<int> order(total);
+    for (int i = 0; i < total; ++i) {
+        cin >> order[i];
+    }
+
+    // machineOf[j][k]：工件 j 的第 k 道工序使用的机器
+    vector<vector<int>> machineOf(n + 1, vector<int>(m));
+
+    // processTime[j][k]：工件 j 的第 k 道工序加工时间
+    vector<vector<int>> processTime(n + 1, vector<int>(m));
+
+    for (int j = 1; j <= n; ++j) {
+        for (int k = 0; k < m; ++k) {
+            cin >> machineOf[j][k];
+        }
+    }
+
+    for (int j = 1; j <= n; ++j) {
+        for (int k = 0; k < m; ++k) {
+            cin >> processTime[j][k];
+        }
+    }
+
+    vector<vector<pair<int, int>>> schedule(m + 1);
+
+    vector<int> cnt(n+1);
+
+    vector<int> jobEnd(n + 1);
+
+    int ans = 0;
+
+    for(int job : order){
+        int k = cnt[job]++;
+        int machine = machineOf[job][k];
+        int duration = processTime[job][k];
+
+        auto &intervals = schedule[machine];
+
+        int ready = jobEnd[job];
+        int start = ready;
+        int current = ready;
+        int intsertpos = intervals.size();
+
+        
+    }
 }
 
 int main() {
