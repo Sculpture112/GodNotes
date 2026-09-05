a88007700c85a7f8700744a0c44717b5d260ae34
xxx <1748701882@qq.com>
2026-09-05 23:39:01 +0800

    vault backup: 2026-09-05 23:39:01

--- God/C_101.cpp
+++ God/C_101.cpp
@@ -9,7 +9,13 @@ const ll LINF = 4e18;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
-
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
 }
 
 int main() {
@@ -17,7 +23,7 @@ int main() {
     cin.tie(nullptr);
 
     int T = 1;
-    // cin >> T;
+    cin >> T;
     while (T--) solve();
 
     return 0;
