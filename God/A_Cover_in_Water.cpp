b569634059147f76b70fa490a76ddabbf869f0ef
xxx <1748701882@qq.com>
2026-09-05 16:37:24 +0800

    vault backup: 2026-09-05 16:37:24

--- God/A_Cover_in_Water.cpp
+++ God/A_Cover_in_Water.cpp
@@ -9,7 +9,29 @@ const ll LINF = 4e18;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int mx = 0;
+    int cnt = 0;
+    for (int i = 0,cur = 0; i < n; i++) {
+        if(s[i] == '.'){
+            cur++;
+            mx = max(cur, mx);
+            cnt++;
+        }
+        else{
+            cur = 0;
+        }
+    }
 
+    if(mx >=3){
+        cout << 2 << "\n";
+    }
+    else{
+        cout << cnt << "\n";
+    }
 }
 
 int main() {
@@ -17,7 +39,7 @@ int main() {
     cin.tie(nullptr);
 
     int T = 1;
-    // cin >> T;
+    cin >> T;
     while (T--) solve();
 
     return 0;
