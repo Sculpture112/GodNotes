04590e22657f531c0278a9c8f3c160b65f4507a4
xxx <1748701882@qq.com>
2026-09-07 14:43:36 +0800

    vault backup: 2026-09-07 14:43:36

--- God/欧拉回路.cpp
+++ God/欧拉回路.cpp
@@ -12,24 +12,55 @@ const int MAXN = 1005;
 int cnt = 1;
 int nxt[MAXN];
 int head[MAXN];
+int to[MAXN];
 
+int indeg[MAXN], outdeg[MAXN];
 
 int n, m;
 
-void addedge(int u,int v){
-
+void addedge(int u, int v)
+{
+    nxt[++cnt] = head[u];
+    head[u] = cnt;
+    to[cnt] = v;
 }
-void solve() {
-    if(n==0)
+void solve()
+{
+    if (n == 0)
         return;
     cin >> m;
 
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        addedge(u, v);
+        outdeg[u]++;
+        indeg[v]++;
+    }
+
+    bool ans = true;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int diff = outdeg[i] - indeg[i];
+        if (diff <= -1 || diff >= 1)
+        {
+            ans = false;
+            break;
+        }
+    }
+
+    cout << ans ? 1 : 0;
+    cout << "\n";
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    while (cin>>n) solve();
+    while (cin >> n)
+        solve();
 
     return 0;
 }
