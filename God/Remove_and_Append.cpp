2be46ecedd3eb1539bd9aa17e5bda7c6a490460e
xxx <1748701882@qq.com>
2026-09-07 11:25:06 +0800

    vault backup: 2026-09-07 11:25:06

--- God/Remove_and_Append.cpp
+++ God/Remove_and_Append.cpp
@@ -4,54 +4,23 @@ using namespace std;
 using ll = long long;
 
 const int INF = 0x3f3f3f3f;
-const ll LINF = 4e18;
+const ll LINF = 1 << 62;
+const double DINF = 1e100;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve()
-{
-    int n, q;
-    cin >> n >> q;
-    vector<int> a(400005);
-    vector<int> qu(q);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
+list<
+void solve() {
 
-    int end = n;
-    for (int i = 0; i < q; i++)
-    {
-        cin >> qu[i];
-        int t = end;
-        for (int j = 0; j <= t; j++)
-        {
-            if (a[j] == qu[i])
-            {
-                swap(a[j], a[end]);
-                end++;
-            }
-        }
-    }
-
-    for (int i = 0; i <= end; i++)
-    {
-        if (a[i] != 0)
-        {
-            cout << a[i] << " ";
-        }
-    }
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--)
-        solve();
+    while (T--) solve();
 
     return 0;
 }
