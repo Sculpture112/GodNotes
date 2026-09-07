ac15bac0662bdcf537f4102ca0a7701a132b197f
xxx <1748701882@qq.com>
2026-09-07 14:34:33 +0800

    vault backup: 2026-09-07 14:34:33

---
+++ God/欧拉回路.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+const int MAXN = 1005;
+int cnt = 1;
+int nxt[MAXN];
+int head[MAXN];
+
+
+int n, m;
+
+void addedge(int u,int v){
+
+}
+void solve() {
+    if(n==0)
+        return;
+    cin >> m;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    while (cin>>n) solve();
+
+    return 0;
+}
