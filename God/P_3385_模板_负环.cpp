04d05b9af1ed13faeee76ae648d3f1922e33aa37
xxx <1748701882@qq.com>
2026-09-09 14:38:34 +0800

    vault backup: 2026-09-09 14:38:34

--- God/P_3385_模板_负环.cpp
+++ God/P_3385_模板_负环.cpp
@@ -25,6 +25,37 @@ void solve()
 
         graph[u].push_back({v, w});
     }
+
+    queue<int> q;
+    vector<bool> inque(n + 1, false);
+    q.push(1);
+    inque[1] = true;
+    vector<ll> dist(n + 1, LINF);
+    vector<int> edgecnt(n + 1);
+    bool ok = false;
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        inque[cur] = false;
+
+        for(auto [to,weight] : graph[cur]){
+            ll newdis = dist[cur] + weight;
+            if(newdis<dist[to]){
+                dist[to] = newdis;
+                edgecnt[to] = edgecnt[cur] + 1;
+                if(edgecnt[to]>=n){
+                    ok = true;
+                }
+
+                if(!inque[to]){
+                    q.push(to);
+                    inque[to] = true;
+                }
+            }
+        }
+    }
+
+    cout << ok ? "NO\n" : "YES\n";
 }
 
 int main()
