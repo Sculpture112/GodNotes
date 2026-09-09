0418b7fe93d1d61bc7bd90937c18de9da4fb0cd7
xxx <1748701882@qq.com>
2026-09-09 17:18:15 +0800

    vault backup: 2026-09-09 17:18:15

---
+++ God/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 1 << 62;
+const double DINF = 1e100;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
