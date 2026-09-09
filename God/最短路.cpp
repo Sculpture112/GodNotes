fbab26c5af1bb7198ea89fa8a35d8e59703caf4f
xxx <1748701882@qq.com>
2026-09-09 11:11:46 +0800

    vault backup: 2026-09-09 11:11:46

--- God/最短路.cpp
+++ God/最短路.cpp
@@ -8,29 +8,57 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 int n, m;
-void solve() {
+struct Edge
+{
+    int to;
+    int weight;
+};
+void solve()
+{
     int a, b, c;
-    vector<pair<int,int>> graph(n + 1);
+    vector<vector<Edge>> graph(n + 1);
 
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++)
+    {
         cin >> a >> b >> c;
-        graph[a].first = b,graph[a].second = c;
+        graph[a].push_back({b, c});
+        graph[b].push_back({a, c});
     }
 
-    int ans = 0;
-    queue<int> q;
-    
-    while(q.empty()){
+    using P = pair<ll, int>;
 
+    vector<ll> dist(n + 1, LINF);
+    priority_queue<P, vector<P>, greater<P>> heap;
+    dist[1] = 0;
+    heap.push({0, 1});
+
+    while (!heap.empty())
+    {
+        auto [cur, u] = heap.top();
+        heap.pop();
+        if (cur > dist[u])
+            continue;
+        if (u == n)
+            break;
+
+        for (auto [to, weight] : graph[u])
+        {
+            if (dist[to] > dist[u] + weight)
+            {
+                dist[to] = dist[u] + weight;
+                heap.push({dist[to], to});
+            }
+        }
     }
-
+    cout << dist[n] << "\n";
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cin >> n >> m;
-    while(n!=0 || m != 0){
+    while (cin >> n >> m && (n != 0 || m != 0))
+    {
         solve();
     }
 
