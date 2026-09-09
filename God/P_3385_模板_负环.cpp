03e3e0ac465078093dd44971bea5fd2875fd7530
xxx <1748701882@qq.com>
2026-09-09 14:28:39 +0800

    vault backup: 2026-09-09 14:28:39

--- God/P_3385_模板_负环.cpp
+++ God/P_3385_模板_负环.cpp
@@ -7,21 +7,35 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
-
-void solve() {
+struct Edge
+{
+    int to;
+    int weight;
+};
+void solve()
+{
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> graph(n + 1);
+    vector<vector<Edge>> graph(n + 1);
 
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        graph[u].push_back({v, w});
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
