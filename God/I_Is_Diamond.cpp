f226b8be88c033b98daeb269b1d27223d6013bb0
xxx <1748701882@qq.com>
2026-09-10 15:37:17 +0800

    vault backup: 2026-09-10 15:37:17

---
+++ God/I_Is_Diamond.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<string> graph(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> graph[i];
+    }
+
+    const int dx[4] = {0, 1, 0, -1};
+    const int dy[4] = {1, 0, -1, 0};
+    int ans = 0;
+    auto check = [&](int x, int y)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            int sum = 0;
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n){
+                continue;
+            }
+            if(nx == '*'){
+                
+            }
+
+        }
+    };
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ()
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
