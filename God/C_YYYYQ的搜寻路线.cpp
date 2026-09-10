d3a3d0c78cdfc49d5433f7addf920fe50c4d19c4
xxx <1748701882@qq.com>
2026-09-10 14:25:45 +0800

    vault backup: 2026-09-10 14:25:45

--- God/C_YYYYQ的搜寻路线.cpp
+++ God/C_YYYYQ的搜寻路线.cpp
@@ -7,9 +7,40 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
-
+int ans = 0;
 void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> graph(n + 1);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
 
+    int t;
+    cin >> t;
+
+    vector<bool> vis(n + 1);
+    queue<int> q;
+    q.push(1);
+    vis[1] = true;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        if(u==t)
+        {
+            ans++;
+            continue;
+        }
+        for(int v:graph[u]){
+            if(!vis[v]){
+                q.push(v);
+                vis[v] = true;
+            }
+        }
+    }
 }
 
 int main() {
