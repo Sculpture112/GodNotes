7a4c5cee97e2763c738feabf531d4e4d1ca50b78
xxx <1748701882@qq.com>
2026-09-10 16:10:22 +0800

    vault backup: 2026-09-10 16:10:22

--- God/K_数组.cpp
+++ God/K_数组.cpp
@@ -9,7 +9,17 @@ const ll LINF = 4e18;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
-    int n
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for(int &x:a){
+        cin >> x;
+    }
+    vector<int> diff(n + 1);
+    for (int i = 0; i < m; i++) {
+        int l, r;
+        cin >> l >> r;
+    }
 }
 
 int main() {
