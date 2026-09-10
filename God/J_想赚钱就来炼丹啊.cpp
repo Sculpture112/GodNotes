890dba44859159f9cfc62502fb009b8eef3d41f3
xxx <1748701882@qq.com>
2026-09-10 16:35:45 +0800

    vault backup: 2026-09-10 16:35:45

--- God/J_想赚钱就来炼丹啊.cpp
+++ God/J_想赚钱就来炼丹啊.cpp
@@ -12,17 +12,28 @@ void solve() {
     int n, m;
     cin >> n >> m;
     vector<int> a(n), b(n), c(n);
+    int mina = INF, minb = INF, minc = INF;
+    map<int, int> mapa,mapb,mapc;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
+        mapa[a[i]]++;
     }
     for (int i = 0; i < n; i++) {
         cin >> b[i];
+        mapb[b[i]]++;
     }
     for (int i = 0; i < n; i++) {
         cin >> c[i];
+        mapc[c[i]]++;
     }
 
-    
+    auto [cura,cnta] = *mapa.begin();
+    auto [curb, cntb] = *mapb.begin();
+    auto [curc, cntc] = *mapc.begin();
+
+    for (int i = 0; i < n;i++){
+        
+    }
 }
 
 int main() {
