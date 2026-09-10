0663b7d68d30f1d0160233c937f01efbb5b7e7ac
xxx <1748701882@qq.com>
2026-09-10 14:15:48 +0800

    vault backup: 2026-09-10 14:15:48

---
+++ God/A_集合的_LCM.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+const int MAXN = 1005;
+int p[MAXN];
+int sz[MAXN];
+vector<vector<int>> order(MAXN);
+int findp(int x)
+{
+    if (p[x] == x)
+    {
+        return x;
+    }
+
+    return findp(p[x]);
+}
+
+void unite(int x, int y)
+{
+    int fx = findp(x);
+    int fy = findp(y);
+
+    if (fx != fy)
+    {
+        p[fx] = fy;
+        sz[fy] = lcm(sz[fx], sz[fy]);
+        order[fy].push_back(sz[fy]);
+    }
+}
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        p[i] = i;
+        sz[i] = i;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        unite(x, y);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        int p = findp(i);
+        cout << *order[p].end();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
---
+++ God/B_小蓝鲸的大迁徙.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    ll N;
+    cin >> N;
+    vector<ll> a(N);
+    vector<ll> b(N);
+    // while (N--)
+    // {
+    //     int x, y;
+    //     cin >> x >> y;
+    //     a.push_back(x);
+    //     b.push_back(y);
+    // }
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a[i] >> b[i];
+    }
+
+    // for (int i = 0; i < N; i++)
+    // {
+    //     cout << a[i] << " ";
+    //     cout << "\n";
+    //     cout << b[i] << " ";
+    // }
+
+    priority_queue<ll, vector<ll>, greater<ll>> heap;
+
+    if (m - n >= 0)
+    {
+        cout << 0 << "\n";
+        return;
+    }
+    int ans = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if(m>=a[i]){
+            heap.push(b[i]);
+        }
+        else{
+            while(!heap.empty() && m < a[i]){
+                ans++;
+                m += heap.top();
+                heap.pop();
+            }
+            if(m>=a[i]){
+                heap.push(b[i]);
+            }
+            else{
+                cout << -1 << "\n";
+                return;
+            }
+        }
+
+    }
+
+    // cout << m;
+    // cout << " " << ans;
+    if (m >= n)
+    {
+        cout << ans << "\n";
+    }
+    else
+    {
+        while (m < n && !heap.empty())
+        {
+            m += heap.top();
+            ans++;
+            heap.pop();
+        }
+        if (m >= n)
+        {
+            cout << ans << "\n";
+        }
+        else
+        {
+            cout << -1 << "\n";
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
