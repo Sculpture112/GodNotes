bb6d5b559b8b8c151f8d9720d608c641464cf3ce
xxx <1748701882@qq.com>
2026-09-10 14:35:58 +0800

    vault backup: 2026-09-10 14:35:58

--- God/F_中正爬山.cpp
+++ God/F_中正爬山.cpp
@@ -11,7 +11,7 @@ const ll LINF = 4e18;
 void solve() {
     int n;
     cin >> n;
-    vector<bool> sig(n + 1);
+    vector<pair<int,int>> sig;
     for (int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
