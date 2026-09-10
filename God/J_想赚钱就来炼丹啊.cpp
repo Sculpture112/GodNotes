e6bea8389c02be25ddd35cfe93697bd8c493f20d
xxx <1748701882@qq.com>
2026-09-10 16:26:11 +0800

    vault backup: 2026-09-10 16:26:11

---
+++ God/J_想赚钱就来炼丹啊.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(n), c(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+    }
+
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
