6a90fcce1c50a0815338fdb2ec1915d0c07c3b19
xxx <1748701882@qq.com>
2026-09-11 14:33:49 +0800

    vault backup: 2026-09-11 14:33:49

---
+++ God/B_3611_模板_传递闭包.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> graph(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> graph[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                graph[i][j] = graph[i][k] | (graph[i][k] && graph[k][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if(j)
+                cout << " ";
+            cout << graph[i][j];
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
