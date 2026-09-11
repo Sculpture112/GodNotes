8c2b3cf70dc1204462e730dda2c1c9e26f975b43
xxx <1748701882@qq.com>
2026-09-11 16:07:37 +0800

    vault backup: 2026-09-11 16:07:37

--- God/P_5250_深基_17_例_5_木材仓库.cpp
+++ God/P_5250_深基_17_例_5_木材仓库.cpp
@@ -8,26 +8,66 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
     vector<int> a;
-    for (int i = 0; i < n; i++) {
-        int op, t;
+    set<ll> wood;
+    for (int i = 0; i < n; i++)
+    {
+        int op;
+        ll t;
         cin >> op >> t;
-        if(op == 1){
-            a.push_back(t);
+        if (op == 1)
+        {
+            if (!wood.insert(t).second)
+            {
+                cout << "Already Exist\n";
+            }
+        }
+        else
+        {
+            if (wood.empty())
+            {
+                cout << "Empty\n";
+                continue;
+            }
+
+            auto right = wood.lower_bound(t);
+            auto chosen = right;
+
+            if (right == wood.begin())
+            {
+                chosen = right;
+            }
+            else if (right == wood.end())
+            {
+                chosen = prev(right);
+            }
+            else
+            {
+                auto left = prev(right);
+                if (t - *left <= *right - t)
+                {
+                    chosen = left;
+                }
+            }
+            cout << *chosen << "\n";
+            wood.erase(chosen);
         }
     }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
