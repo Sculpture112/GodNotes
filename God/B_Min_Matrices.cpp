4fba9258858f42b9f7ec67cdf2fd72badcab4134
xxx <1748701882@qq.com>
2026-09-12 22:58:23 +0800

    vault backup: 2026-09-12 22:58:23

---
+++ God/B_Min_Matrices.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    if (k < n || k >= n + 4)
+    {
+        cout << -1 << "\n";
+        return;
+    }
+    vector<vector<int>> graph(n, vector<int>(n,0));
+
+    for (int i = 0; i < n;i++){
+        graph[i][0] = i + 1;
+    }
+
+    for (int i = 1; i <= n;i++){
+        graph[0][i] = n + i;
+    }
+
+    for(int i = )
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
