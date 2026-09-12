b0e5e062bfa3aaf43c9e995c27cc3caef2073e2f
xxx <1748701882@qq.com>
2026-09-12 23:25:20 +0800

    vault backup: 2026-09-12 23:25:20

--- God/B_Min_Matrices.cpp
+++ God/B_Min_Matrices.cpp
@@ -13,7 +13,7 @@ void solve()
     int n, k;
     cin >> n >> k;
 
-    if (k < n || k > n + n-1)
+    if (k < n || k > n + n - 1)
     {
         cout << -1 << "\n";
         return;
@@ -30,7 +30,21 @@ void solve()
     //     }
     // }
 
-    
+    vector<vector<int>> graph(n, vector<int>(n, 0));
+    for (int i = 0; i < k; i++)
+    {
+        graph[i][i] = i + 1;
+    }
+
+    //     for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         if(graph[i][j] ==0){
+    //             graph[i][j] = ++k;
+    //         }
+    //     }
+    // }
 }
 
 int main()
