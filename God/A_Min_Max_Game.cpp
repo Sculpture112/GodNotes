3bd6015ad57a0b04e52a51d3bd98ab09e75f4e83
xxx <1748701882@qq.com>
2026-09-12 22:42:46 +0800

    vault backup: 2026-09-12 22:42:46

--- God/A_Min_Max_Game.cpp
+++ God/A_Min_Max_Game.cpp
@@ -8,17 +8,40 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
-
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int one = 0;
+    int zero = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] == 0)
+            zero++;
+        else
+        {
+            one++;
+        }
+    }
+    if(one>=zero){
+        cout << "Bessie" << "\n";
+    }
+    else{
+        cout << "Elsie" << "\n";
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    cin >> T;
+    while (T--)
+        solve();
 
     return 0;
 }
