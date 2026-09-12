70b3c527dee7db12bb364a83e7ff07710f54ee3a
xxx <1748701882@qq.com>
2026-09-12 20:43:25 +0800

    vault backup: 2026-09-12 20:43:25

--- God/Walk_the_Line.cpp
+++ God/Walk_the_Line.cpp
@@ -8,18 +8,39 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n, s, l;
     cin >> n >> s >> l;
+
+    vector<ll> a(n);
+    a[0] = LINF;
+    for (int i = 1; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    ll left, right;
+    
+    left = a[s - 1], right = a[s + 1];
+    for (int i = s; i < n;)
+    {
+        if(left<=right){
+
+        }
+
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
