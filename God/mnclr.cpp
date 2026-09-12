e12f34db27280fef4a6309ada8639cc8bd66a97b
xxx <1748701882@qq.com>
2026-09-12 20:04:19 +0800

    vault backup: 2026-09-12 20:04:19

---
+++ God/mnclr.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    string s;
+    cin >> s;
+
+    cout << s[0];
+    for (int i = 1; i < s.size(); i++) {
+        cout << 'o' << s[i];
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
