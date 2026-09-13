4edcaf536460f8b98fa2c10661cfc860ed5b310b
xxx <1748701882@qq.com>
2026-09-13 19:17:31 +0800

    vault backup: 2026-09-13 19:17:30

--- God/小月的纪录点.cpp
+++ God/小月的纪录点.cpp
@@ -8,34 +8,45 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
-    vector<int> a(n);
-    int mx = 0;
-    int last = 0;
+    vector<ll> a(n);
+    ll mx = 0;
+    int last = INF;
     int dis = 0;
     int cnt = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> a[i];
-        if(mx<a[i]){
+        if (mx < a[i])
+        {
+            // cout << "Yes";
             mx = max(mx, a[i]);
-            dis = max(dis, i-last);
+            dis = max(dis, i - last);
             last = i;
             cnt++;
         }
     }
 
+    if (cnt < 2)
+    {
+        cout << 1 << " " << 0;
+        return;
+    }
     cout << cnt << " " << dis;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
