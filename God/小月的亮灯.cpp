dc89f561e0fd3a39c2b7d5fc13554722b3dc9d93
xxx <1748701882@qq.com>
2026-09-13 19:03:58 +0800

    vault backup: 2026-09-13 19:03:58

---
+++ God/小月的亮灯.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 4e18;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve()
+{
+    int a[3] = {};
+    int cnt = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> a[i];
+        if(a[i]==1){
+            cnt+=i;
+        }
+    }
+    cout << cnt;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
