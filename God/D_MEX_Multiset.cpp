b37f6f2e41abec204a4043e49827abfa5b1fe3ae
xxx <1748701882@qq.com>
2026-09-20 16:56:45 +0800

    vault backup: 2026-09-20 16:56:45

--- God/D_MEX_Multiset.cpp
+++ God/D_MEX_Multiset.cpp
@@ -8,17 +8,56 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int zero = 0;
+    for (int &x : a)
+    {
+        cin >> x;
+        if (x == 0)
+        {
+            zero++;
+        }
+    }
 
+    if (zero == 1)
+    {
+        cout << "NO\n";
+        return;
+    }
+    string ans(n, 'A');
+
+    bool firstzero = false;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 0 && !firstzero)
+        {
+
+            firstzero = true;
+            ans[i] = 'B';
+        }
+        if (a[i] == 0)
+        {
+            ans[i] = 'C';
+        }
+    }
+    cout << "YES\n";
+    cout << ans << "\n";
+    
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    cin >> T;
+    while (T--)
+        solve();
 
     return 0;
 }
