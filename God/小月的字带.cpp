8fe6ded1256656f5ec0e4964d9fc9ac460731a53
xxx <1748701882@qq.com>
2026-09-20 20:22:16 +0800

    vault backup: 2026-09-20 20:22:16

--- God/小月的字带.cpp
+++ God/小月的字带.cpp
@@ -8,7 +8,8 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
     char c;
@@ -16,26 +17,32 @@ void solve() {
     int cnt = 0;
     char last = c;
     char ch;
-    for (int i = 0; i < n;i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> ch;
-        if (ch == last){
-            cnt+=2;
-        }
-        else{
+        if (ch == last)
+        {
+            cnt *= 2;
             cnt++;
+        }
+        else
+        {
+            cnt *= 2;
         }
         last = ch;
     }
-    cout << cnt * 2;
+    cout << cnt*2;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     // cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
