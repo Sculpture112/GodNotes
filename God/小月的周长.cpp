07a46b5f34a14a733abcde7f3bd824edc5565222
xxx <1748701882@qq.com>
2026-09-20 19:30:19 +0800

    vault backup: 2026-09-20 19:30:18

--- God/小月的周长.cpp
+++ God/小月的周长.cpp
@@ -8,17 +8,41 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
+    int n, m;
+    int x, y;
+    cin >> n >> m;
+    cin >> x >> y;
+    int sum = 2 * n + 2 * m;
+    if ((x == n || x == 1) && (y > 1 && y < n))
+    {
+        cout << sum + 2 << "\n";
+        return;
+    }
+    if ((y == 1 || y == n) && (x > 1 && x < n))
+    {
+        cout << sum + 2 << "\n";
+        return;
+    }
 
+    if ((x > 1 && x < n && y > 1 && y < n))
+    {
+        cout << sum + 4 << "\n";
+        return;
+    }
+    cout << sum << "\n";
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    cin >> T;
+    while (T--)
+        solve();
 
     return 0;
 }
