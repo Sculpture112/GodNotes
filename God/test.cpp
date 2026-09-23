458adc74f60c6102d60ae65aecca469eeb83d0c0
xxx <1748701882@qq.com>
2026-09-23 19:48:16 +0800

    vault backup: 2026-09-23 19:48:16

--- God/test.cpp
+++ God/test.cpp
@@ -3,23 +3,8 @@ using namespace std;
 
 using ll = long long;
 
-const int INF = 0x3f3f3f3f;
-const ll LINF = 1LL << 62;
-const double DINF = 1e100;
-
-#define all(x) (x).begin(), (x).end()
-
-void solve() {
-
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int T = 1;
-    // cin >> T;
-    while (T--) solve();
-
-    return 0;
+int main(){
+    int a;
+    cin >> a;
+    cout << a;
 }
