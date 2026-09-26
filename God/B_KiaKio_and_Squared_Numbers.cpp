985c02e1d1a60e9ea8a3af0ae3e4ff963e216ac1
xxx <1748701882@qq.com>
2026-09-26 23:08:51 +0800

    vault backup: 2026-09-26 23:08:51

--- God/B_KiaKio_and_Squared_Numbers.cpp
+++ God/B_KiaKio_and_Squared_Numbers.cpp
@@ -8,8 +8,41 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
+ll upgrade(int x){
+    ll sum = 0;
+    while(x>0){
+        sum += (x % 10) * (x % 10);
+        x /= 10;
+    }
+    return sum;
+}
+ll C2(int x){
+    if(x<2)
+        return 0;
+    return x * (x - 1) / 2;
+}
 void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int&x:a)
+        cin >> x;
 
+    for (int i = 0; i < 100;i++){
+        for (int i = 0; i < n;i++){
+            a[i] = upgrade(a[i]);
+        }
+    }
+    unordered_map<int,int> cnt;
+    for (int i = 0; i < n;i++){
+        cnt[a[i]]++;
+    }
+
+    ll ans = 0;
+    for(auto[_,cn]:cnt){
+        ans += C2(cn);
+    }
+    cout << ans << "\n";
 }
 
 int main() {
@@ -17,7 +50,7 @@ int main() {
     cin.tie(nullptr);
 
     int T = 1;
-    // cin >> T;
+    cin >> T;
     while (T--) solve();
 
     return 0;
