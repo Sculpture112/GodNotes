afb4a0612c7462f8891ecdc070785f9ca1a057e1
xxx <1748701882@qq.com>
2026-09-26 19:49:24 +0800

    vault backup: 2026-09-26 19:49:24

--- God/C_Maximize_the_Score.cpp
+++ God/C_Maximize_the_Score.cpp
@@ -8,17 +8,45 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> cnt(n + 1, 2);
+    ll sum = 0;
+    vector<int> a(2 * n);
+    for (int &x : a)
+        cin >> x;
 
+    int start = 0;
+
+    while (start != 2 * n)
+    {
+        int end = lower_bound(a.begin() + start + 1, a.end(), a[start]) - a.begin();
+
+        if (end == 2 * n)
+        {
+            sum += 1;
+        }
+        else
+        {
+            sum += (end - start + 1) * (end - start + 1);
+        }
+
+        start = end + 1;
+    }
+    cout << sum << "\n";
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
-    // cin >> T;
-    while (T--) solve();
+    cin >> T;
+    while (T--)
+        solve();
 
     return 0;
 }
