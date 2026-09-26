3e0039971585248481626adca8581727fd0e65a1
xxx <1748701882@qq.com>
2026-09-26 21:06:33 +0800

    vault backup: 2026-09-26 21:06:33

--- God/A_Threshold_Movement.cpp
+++ God/A_Threshold_Movement.cpp
@@ -8,24 +8,38 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n;
     cin >> n;
     vector<int> a(n);
-    for(int&x:a)
+    for (int &x : a)
         cin >> x;
     sort(a.begin(), a.end());
+    if (n < 2)
+    {
+        cout << "YES\n";
+    }
     int diff = a[n / 2] - a[n / 2 - 1];
-    if(diff>)
+    if (diff >= 1)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
