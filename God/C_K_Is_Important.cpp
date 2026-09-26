7211018b9717c9af68c64a5397db020b7c1ffca9
xxx <1748701882@qq.com>
2026-09-26 23:32:07 +0800

    vault backup: 2026-09-26 23:32:07

--- God/C_K_Is_Important.cpp
+++ God/C_K_Is_Important.cpp
@@ -8,35 +8,38 @@ const ll LINF = 4e18;
 
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve()
+{
     int n, k;
     cin >> n >> k;
-    vector<int> a(n+1);
-    for (int i = 1; i <= n; i++) {
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
         cin >> a[i];
     }
 
     ll ans = 0;
     int m = n;
     int pos = m - k + 1;
-    while(m>=k){
-        if(a[pos]>=a[m]){
-            ans += a[pos];
-            m--;
-        }
-        else{
-            
-        }
+
+    vector<ll> sum(n + 2);
+    for (int i = n; i >= 1; i--)
+    {
+        sum[i] = sum[i + 1] + a[i];
     }
+
+    ans = sum[n - k - 1];
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     cin >> T;
-    while (T--) solve();
+    while (T--)
+        solve();
 
     return 0;
 }
